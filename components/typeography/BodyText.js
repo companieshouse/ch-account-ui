283c3176ea08d8879ab70ccdfd7bf0327658f78c
robevans <rob@irrelon.com>
2021-02-16 12:38:23 +0000

    BodyText.js prop types

--- components/typeography/BodyText.js
+++ components/typeography/BodyText.js
@@ -1,15 +1,30 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
-const BodyText = ({ label = '', children, hint = false, className = '' }) => {
+const BodyText = ({ children, hint = false, className = '', weight = 'regular' }) => {
   const classes = [className]
 
   if (hint === true) classes.push('govuk-hint')
+  if (weight === 'bold') classes.push('govuk-!-font-weight-bold')
 
   const finalClassName = classes.join(' ').trim()
 
   return (
-    <p className={`govuk-body ${finalClassName}`}>{label}{children}</p>
+    <p className={`govuk-body ${finalClassName}`}>{children}</p>
   )
 }
 
 export default BodyText
+
+BodyText.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  hint: PropTypes.bool,
+  weight: PropTypes.string
+}
+
+BodyText.defaultProps = {
+  className: '',
+  hint: false,
+  weight: 'regular'
+}
