f97e7e42015706daffbac3110d5adca40e65accd
robevans <rob@irrelon.com>
2021-02-16 12:36:09 +0000

    InputField.js prop types

--- components/interaction/InputField.js
+++ components/interaction/InputField.js
@@ -1,8 +1,13 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import { getFieldError } from '../../services/errors'
 
-const InputField = ({ label = '', id, className = '', errors = [] }) => {
+const InputField = ({ label = '', type = 'text', id, className = '', errors = [], hint = '', fixedWidth = '', fluidWidth = '' }) => {
   const classes = [className]
+
+  if (fixedWidth) classes.push(`govuk-input--width-${fixedWidth}`)
+  if (fluidWidth) classes.push(`govuk-!-width-${fluidWidth}`)
+
   const finalClassName = classes.join(' ').trim()
   const error = getFieldError(errors, id)
 
@@ -11,13 +16,14 @@ const InputField = ({ label = '', id, className = '', errors = [] }) => {
       <label className="govuk-label" htmlFor={id}>
         {label}
       </label>
+      {Boolean(hint) && <div id={`${id}-hint`} className="govuk-hint">{hint}</div>}
       {Boolean(error) && <span id={`${id}-error`} className="govuk-error-message">
         <span className="govuk-visually-hidden">Error:</span> {error.label}
       </span>}
       <input className={`govuk-input ${Boolean(error) && 'govuk-input--error'} ${finalClassName}`}
              id={id}
              name={id}
-             type="text"
+             type={type}
              aria-describedby={Boolean(error) && `${id}-error`}
       />
     </React.Fragment>
@@ -25,3 +31,27 @@ const InputField = ({ label = '', id, className = '', errors = [] }) => {
 }
 
 export default InputField
+
+InputField.propTypes = {
+  className: PropTypes.string,
+  errors: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    anchor: PropTypes.string
+  })),
+  fixedWidth: PropTypes.string,
+  fluidWidth: PropTypes.string,
+  hint: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  type: PropTypes.string
+}
+
+InputField.defaultProps = {
+  className: '',
+  errors: [],
+  fixedWidth: '',
+  fluidWidth: '',
+  hint: '',
+  label: '',
+  type: 'text'
+}
