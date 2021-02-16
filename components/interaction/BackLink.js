f5c97d1de784315f17ab6201e55be6e46131760b
robevans <rob@irrelon.com>
2021-02-16 12:24:03 +0000

    Updated BackLink to work with either JS or non-JS environments

--- components/interaction/BackLink.js
+++ components/interaction/BackLink.js
@@ -1,15 +1,38 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import Link from 'next/link'
 
-const BackLink = ({ children, href = '/', className = '' }) => {
+const BackLink = ({ children, href = '', className = '' }) => {
   const classes = [className]
   const finalClassName = classes.join(' ').trim()
 
+  if (href) {
+    return (
+      <Link href={href}>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a className={`govuk-back-link no-js-hide ${finalClassName}`}>{children}</a>
+      </Link>
+    )
+  }
+
   return (
-    <Link href={href}>
-      <a className={`govuk-back-link no-js-hide ${finalClassName}`}>{children}</a>
-    </Link>
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
+    <a href="#" onClick={() => {
+      window.history.back()
+      return false
+    }} className={`govuk-back-link no-js-hide ${finalClassName}`}>{children}</a>
   )
 }
 
 export default BackLink
+
+BackLink.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  href: PropTypes.string
+}
+
+BackLink.defaultProps = {
+  className: '',
+  href: ''
+}
