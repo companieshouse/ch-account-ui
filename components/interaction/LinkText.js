1685b7d4d5a2569dd0cc496c8cb80e0a21f9b682
robevans <rob@irrelon.com>
2021-02-16 12:37:00 +0000

    LinkText.js prop types

--- components/interaction/LinkText.js
+++ components/interaction/LinkText.js
@@ -1,15 +1,27 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import Link from 'next/link'
 
-const LinkText = ({ children, href = '/', className = '' }) => {
+const LinkText = ({ children, href, className = '' }) => {
   const classes = [className]
   const finalClassName = classes.join(' ').trim()
 
   return (
     <Link href={href}>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a className={`govuk-link ${finalClassName}`}>{children}</a>
     </Link>
   )
 }
 
 export default LinkText
+
+LinkText.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  href: PropTypes.string.isRequired
+}
+
+LinkText.defaultProps = {
+  className: ''
+}
