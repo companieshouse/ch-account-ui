2fdeb9f790e4df6a1e738298cd5a1d058174aee5
robevans <rob@irrelon.com>
2021-02-17 18:50:34 +0000

    Moved login and home to use view-based components for pages so that in the future, the pages are decoupled from the state that might drive them, such that they can be rendered on client or server, and with or without Next.js

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,62 +1,46 @@
 import React from 'react'
-import WidthContainer from '../../components/general-ui/layout/WidthContainer'
-import BackLink from '../../components/general-ui/interaction/BackLink'
-import Main from '../../components/general-ui/layout/Main'
-import Column from '../../components/general-ui/layout/Column'
-import Row from '../../components/general-ui/layout/Row'
-import ErrorSummary from '../../components/general-ui/typeography/ErrorSummary'
-import HeadingText from '../../components/general-ui/typeography/HeadingText'
-import FormGroup from '../../components/general-ui/interaction/FormGroup'
-import InputField from '../../components/general-ui/interaction/InputField'
-import BodyText from '../../components/general-ui/typeography/BodyText'
-import Button from '../../components/general-ui/interaction/Button'
-import LinkText from '../../components/general-ui/interaction/LinkText'
-import HeadingCount from '../../services/HeadingCount'
+import LoginIndexView from '../../components/views/login/IndexView'
+import { login } from '../../services/forgerock'
 
-const Login = ({ formAction = '/api/v1.0/login', onSubmit, errors = [] }) => {
-  const headingCount = new HeadingCount()
+const Login = () => {
+  const [errors, setErrors] = React.useState([])
+  const onLoginSubmit = (evt) => {
+    evt.preventDefault()
 
-  React.useEffect(() => {
-    headingCount.reset()
-  })
+    setErrors([])
+
+    const username = evt.target.elements.username.value
+    const password = evt.target.elements.password.value
+
+    login({ username, password }).then((response) => {
+      console.log('Response', response)
+    }).catch((err) => {
+      const message = err?.payload?.message || 'Login failure'
+      const reason = err?.payload?.reason || 'Unknown'
+      const newErrors = []
+
+      switch (reason) {
+        case 'Unauthorised':
+          newErrors.push({
+            label: message,
+            anchor: 'username'
+          })
+          break
+
+        default:
+          newErrors.push({
+            label: message,
+            anchor: 'username'
+          })
+          break
+      }
+
+      setErrors(newErrors)
+    })
+  }
 
   return (
-    <WidthContainer>
-      <BackLink>Back</BackLink>
-      <Main>
-        <Row>
-          <Column type='two-thirds'>
-            <form action={formAction} onSubmit={onSubmit} method="post">
-              {errors.length === 0 && <HeadingText headingCount={headingCount}>Sign in to your Companies House account</HeadingText>}
-              {errors.length > 0 && <>
-                <ErrorSummary headingCount={headingCount} title="There is a problem" errors={errors}/>
-                <HeadingText headingCount={headingCount}>Sign in to your Companies House account</HeadingText>
-              </>}
-
-              <FormGroup errors={errors} groupIds={['username']}>
-                <InputField id="username" type="text" autoComplete="email" label="Email address" errors={errors} />
-              </FormGroup>
-
-              <FormGroup errors={errors} groupIds={['password']}>
-                <InputField id="password" type="password" label="Password" errors={errors} />
-              </FormGroup>
-
-              <Button type="submit" className="govuk-button" data-module="govuk-button">
-                Sign in
-              </Button>
-
-              <BodyText>
-                <LinkText href="/register/provide-contact-details">Register a new account</LinkText>
-              </BodyText>
-
-              <BodyText>
-                <LinkText href="/password-recovery/request">I&apos;ve forgotten my password</LinkText>
-              </BodyText>
-            </form>
-          </Column>
-        </Row>
-      </Main>
-    </WidthContainer>
+    <LoginIndexView onSubmit={onLoginSubmit} errors={errors} />
   )
 }
 
