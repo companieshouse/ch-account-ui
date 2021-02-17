ab9ca34820e888ecc33b789d30149052cacf538e
robevans <rob@irrelon.com>
2021-02-17 18:50:34 +0000

    Moved login and home to use view-based components for pages so that in the future, the pages are decoupled from the state that might drive them, such that they can be rendered on client or server, and with or without Next.js

---
+++ components/views/login/IndexView.js
@@ -0,0 +1,76 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import WidthContainer from '../../../components/general-ui/layout/WidthContainer'
+import BackLink from '../../../components/general-ui/interaction/BackLink'
+import Main from '../../../components/general-ui/layout/Main'
+import Column from '../../../components/general-ui/layout/Column'
+import Row from '../../../components/general-ui/layout/Row'
+import ErrorSummary from '../../../components/general-ui/typeography/ErrorSummary'
+import HeadingText from '../../../components/general-ui/typeography/HeadingText'
+import FormGroup from '../../../components/general-ui/interaction/FormGroup'
+import InputField from '../../../components/general-ui/interaction/InputField'
+import BodyText from '../../../components/general-ui/typeography/BodyText'
+import Button from '../../../components/general-ui/interaction/Button'
+import LinkText from '../../../components/general-ui/interaction/LinkText'
+import HeadingCount from '../../../services/HeadingCount'
+
+const LoginIndexView = ({ formAction = '/api/v1.0/login', onSubmit, errors = [] }) => {
+  const headingCount = new HeadingCount()
+
+  React.useEffect(() => {
+    headingCount.reset()
+  })
+
+  return (
+    <WidthContainer>
+      <BackLink>Back</BackLink>
+      <Main>
+        <Row>
+          <Column type='two-thirds'>
+            <form action={formAction} onSubmit={onSubmit} method="post">
+              {errors.length === 0 &&
+              <HeadingText headingCount={headingCount}>Sign in to your Companies House account</HeadingText>}
+              {errors.length > 0 && <>
+                <ErrorSummary headingCount={headingCount} title="There is a problem" errors={errors}/>
+                <HeadingText headingCount={headingCount}>Sign in to your Companies House account</HeadingText>
+              </>}
+
+              <FormGroup errors={errors} groupIds={['username']}>
+                <InputField id="username" type="text" autoComplete="email" label="Email address" errors={errors}/>
+              </FormGroup>
+
+              <FormGroup errors={errors} groupIds={['password']}>
+                <InputField id="password" type="password" label="Password" errors={errors}/>
+              </FormGroup>
+
+              <Button type="submit" className="govuk-button" data-module="govuk-button">
+                Sign in
+              </Button>
+
+              <BodyText>
+                <LinkText href="/register/provide-contact-details">Register a new account</LinkText>
+              </BodyText>
+
+              <BodyText>
+                <LinkText href="/password-recovery/request">I&apos;ve forgotten my password</LinkText>
+              </BodyText>
+            </form>
+          </Column>
+        </Row>
+      </Main>
+    </WidthContainer>
+  )
+}
+
+export default LoginIndexView
+
+LoginIndexView.propTypes = {
+  errors: PropTypes.array,
+  formAction: PropTypes.string,
+  onSubmit: PropTypes.func
+}
+
+LoginIndexView.defaultProps = {
+  errors: [],
+  formAction: '/api/v1.0/login'
+}
--- pages/account/home.js
+++ pages/account/home.js
@@ -1,80 +1,9 @@
 import React from 'react'
-import WidthContainer from '../../components/general-ui/layout/WidthContainer'
-import BackLink from '../../components/general-ui/interaction/BackLink'
-import Main from '../../components/general-ui/layout/Main'
-import Column from '../../components/general-ui/layout/Column'
-import Row from '../../components/general-ui/layout/Row'
-import ErrorSummary from '../../components/general-ui/typeography/ErrorSummary'
-import HeadingText from '../../components/general-ui/typeography/HeadingText'
-import BodyText from '../../components/general-ui/typeography/BodyText'
-import Button from '../../components/general-ui/interaction/Button'
-import HeadingCount from '../../services/HeadingCount'
-import List from '../../components/general-ui/typeography/List'
-import ListItem from '../../components/general-ui/typeography/ListItem'
-import LinkText from '../../components/general-ui/interaction/LinkText'
-import CompanySummary from '../../components/application-specific/CompanySummary'
+import AccountHomeView from '../../components/views/account/Home'
 
-const AccountHome = ({
-  errors = [], userDetails = {
-    fullName: 'Test User',
-    emailAddress: 'test@user.com'
-  },
-  companies = [{
-    name: 'Test Company',
-    number: '0123456789',
-    address: '2nd Floor Red House, 17 London Road, London, SA73 8PH',
-    personsAuthorisedToFile: [{
-      name: 'Test User',
-      emailAddress: 'test@user.com',
-      permissions: ['FILE_ACCOUNTS', 'FILE_CONFIRMATION_STATEMENTS', 'MAKE_CHANGES_TO_THE_COMPANY'],
-      canAuthOthers: true
-    }]
-  }]
-}) => {
-  const { fullName } = userDetails
-  const headingCount = new HeadingCount()
-
-  React.useEffect(() => {
-    headingCount.reset()
-  })
-
+const AccountHome = () => {
   return (
-    <WidthContainer>
-      <BackLink>Back</BackLink>
-      <Main>
-        <Row>
-          <Column type='full'>
-            {errors.length === 0 && <HeadingText headingCount={headingCount} size="l" caption='Home'>{fullName}</HeadingText>}
-            {errors.length > 0 && <>
-              <ErrorSummary headingCount={headingCount} title="There is a problem" errors={errors}/>
-              <HeadingText headingCount={headingCount} size="l" caption='Home'>{fullName}</HeadingText>
-            </>}
-
-            <HeadingText headingCount={headingCount} size="s">Your companies</HeadingText>
-            <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible" />
-
-            {companies.length === 0 && <>
-              <HeadingText headingCount={headingCount} size="m">You have not added any companies to this account</HeadingText>
-              <BodyText>
-                To file online for a company, you must add it to your account.
-              </BodyText>
-              <BodyText>
-                You&apos;ll need:
-              </BodyText>
-              <List>
-                <ListItem>the company number</ListItem>
-                <ListItem>the company <LinkText href="https://www.gov.uk/guidance/company-authentication-codes-for-online-filing">authentication code</LinkText></ListItem>
-              </List>
-            </>}
-            {companies.map((company) => <CompanySummary key={company} company={company} headingCount={headingCount} />)}
-
-            <Button type="link" className="govuk-button" data-module="govuk-button">
-              Add a company
-            </Button>
-          </Column>
-        </Row>
-      </Main>
-    </WidthContainer>
+    <AccountHomeView />
   )
 }
 
