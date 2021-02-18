20827a19041ce0087ebb935156a74e29b8603daf
robevans <rob@irrelon.com>
2021-02-18 17:01:56 +0000

    Fixed Column component className interfering with width

--- components/general-ui/layout/Column.js
+++ components/general-ui/layout/Column.js
@@ -3,15 +3,15 @@ import React from 'react'
 const Column = ({ children, width = 'full', className = '' }) => {
   const classes = [className]
 
-  if (width === 'full') classes.push('full')
-  if (width === 'one-half') classes.push('one-half')
-  if (width === 'one-third') classes.push('one-third')
-  if (width === 'two-thirds') classes.push('two-thirds')
+  if (width === 'full') classes.push('govuk-grid-column-full')
+  if (width === 'one-half') classes.push('govuk-grid-column-one-half')
+  if (width === 'one-third') classes.push('govuk-grid-column-one-third')
+  if (width === 'two-thirds') classes.push('govuk-grid-column-two-thirds')
 
   const finalClassName = classes.join(' ').trim()
 
   return (
-    <div className={`govuk-grid-column-${finalClassName}`}>
+    <div className={finalClassName}>
       {children}
     </div>
   )
