e5f9f7136c1ec159681bfaac783bc244bf254b44
robevans <rob@irrelon.com>
2021-02-18 17:02:22 +0000

    Added SectionBreak.js component

---
+++ components/general-ui/typeography/SectionBreak.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SectionBreak = () => {
+  return (
+    <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible" />
+  )
+}
+
+export default SectionBreak
