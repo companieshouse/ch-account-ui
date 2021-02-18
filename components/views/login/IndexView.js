631cc14678b42fc4fe1aa4598bf5d6338c11552f
robevans <rob@irrelon.com>
2021-02-18 14:27:07 +0000

    Proptypes

--- components/views/login/IndexView.js
+++ components/views/login/IndexView.js
@@ -7,14 +7,12 @@ import Column from '../../../components/general-ui/layout/Column'
 import Row from '../../../components/general-ui/layout/Row'
 import ErrorSummary from '../../../components/general-ui/typeography/ErrorSummary'
 import HeadingText from '../../../components/general-ui/typeography/HeadingText'
-import FormGroup from '../../../components/general-ui/interaction/FormGroup'
-import InputField from '../../../components/general-ui/interaction/InputField'
 import BodyText from '../../../components/general-ui/typeography/BodyText'
 import Button from '../../../components/general-ui/interaction/Button'
 import LinkText from '../../../components/general-ui/interaction/LinkText'
 import NameCallback from '../../forgerock-ui/NameCallback'
-import { getCallbackElementData } from '../../../services/forgerock'
 import PasswordCallback from '../../forgerock-ui/PasswordCallback'
+import HeadingCount from '../../../services/HeadingCount'
 
 const LoginIndexView = ({ formAction = '/api/v1.0/login', onSubmit, errors = [], uiElements = [], headingCount }) => {
   return (
@@ -31,7 +29,8 @@ const LoginIndexView = ({ formAction = '/api/v1.0/login', onSubmit, errors = [],
                 <HeadingText headingCount={headingCount}>Sign in to your Companies House account</HeadingText>
               </>}
 
-              {uiElements.map((uiElement, index) => {
+              {uiElements.map((uiElementPayload, index) => {
+                const uiElement = uiElementPayload.payload
                 console.log('Processing ui element', uiElement)
                 switch (uiElement.type) {
                   case 'NameCallback':
@@ -69,10 +68,13 @@ export default LoginIndexView
 LoginIndexView.propTypes = {
   errors: PropTypes.array,
   formAction: PropTypes.string,
-  onSubmit: PropTypes.func
+  onSubmit: PropTypes.func,
+  uiElements: PropTypes.array,
+  headingCount: PropTypes.instanceOf(HeadingCount)
 }
 
 LoginIndexView.defaultProps = {
   errors: [],
-  formAction: '/api/v1.0/login'
+  formAction: '/api/v1.0/login',
+  uiElements: []
 }
