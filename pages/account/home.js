5aa9e040c9f77901563a97e0bf8d54323bfacc00
robevans <rob@irrelon.com>
2021-02-18 11:43:26 +0000

    Moved login and home to use view-based components for pages so that in the future, the pages are decoupled from the state that might drive them, such that they can be rendered on client or server, and with or without Next.js

--- pages/account/home.js
+++ pages/account/home.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import AccountHomeView from '../../components/views/account/Home'
+import HeadingCount from '../../services/HeadingCount'
 
 const AccountHome = () => {
+  const headingCount = new HeadingCount()
+
+  React.useEffect(() => {
+    headingCount.reset()
+  })
+
+  const data = {
+    errors: [],
+    userDetails: {
+      fullName: 'Test User',
+      emailAddress: 'test@user.com'
+    },
+    companies: [{
+      name: 'Test Company',
+      number: '0123456789',
+      address: '2nd Floor Red House, 17 London Road, London, SA73 8PH',
+      personsAuthorisedToFile: [{
+        name: 'Test User',
+        emailAddress: 'test@user.com',
+        permissions: ['FILE_ACCOUNTS', 'FILE_CONFIRMATION_STATEMENTS', 'MAKE_CHANGES_TO_THE_COMPANY'],
+        canAuthOthers: true
+      }]
+    }]
+  }
+
   return (
-    <AccountHomeView />
+    <AccountHomeView headingCount={headingCount} errors={data.errors} companies={data.companies} userDetails={data.userDetails} />
   )
 }
 
