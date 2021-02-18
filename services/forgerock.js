967f4f7f4f66d646feb1b686a5705596b840312c
robevans <rob@irrelon.com>
2021-02-18 14:25:26 +0000

    Re-coded login flow to handle dynamic ui based on forgerock auth tree

--- services/forgerock.js
+++ services/forgerock.js
@@ -1,4 +1,4 @@
-import { Config, FRAuth, TokenManager, UserManager, StepType } from '@forgerock/javascript-sdk'
+import { Config, FRAuth, TokenManager, StepType } from '@forgerock/javascript-sdk'
 import {
   FORGEROCK_AM,
   FORGEROCK_CLIENT_ID,
@@ -8,86 +8,73 @@ import {
   FORGEROCK_TREE_LOGIN
 } from './environment'
 
-export const login = ({ username, password }) => {
-  return new Promise((resolve, reject) => {
-    Config.set({
-      clientId: FORGEROCK_CLIENT_ID,
-      realmPath: FORGEROCK_REALM,
-      redirectUri: FORGEROCK_REDIRECT,
-      scope: FORGEROCK_SCOPE,
-      serverConfig: {
-        baseUrl: FORGEROCK_AM,
-        timeout: 30000
-      },
-      tree: FORGEROCK_TREE_LOGIN
-    })
+export const loginFlow = ({
+  onSuccess,
+  onFailure,
+  onUpdateUi
+}) => {
+  Config.set({
+    clientId: FORGEROCK_CLIENT_ID,
+    realmPath: FORGEROCK_REALM,
+    redirectUri: FORGEROCK_REDIRECT,
+    scope: FORGEROCK_SCOPE,
+    serverConfig: {
+      baseUrl: FORGEROCK_AM,
+      timeout: 30000
+    },
+    tree: FORGEROCK_TREE_LOGIN
+  })
 
-    const handleFatalError = (err) => {
-      console.log('ForgeRock fatal error', err)
-      reject(err)
-    }
+  const handleFatalError = (err) => {
+    console.log('ForgeRock fatal error', err)
+    onFailure(err)
+  }
 
-    const nextStep = (step) => {
-      console.log('ForgeRock calling next step', step)
-      FRAuth.next(step).then(handleStep).catch(handleFatalError)
-    }
+  const nextStep = (step) => {
+    console.log('ForgeRock calling next step', step)
+    FRAuth.next(step).then(handleStep).catch(handleFatalError)
+  }
 
-    const handleStep = (step) => {
-      if (step.type === StepType.LoginSuccess) {
-        console.log('ForgeRock login success', step)
-        console.log('ForgeRock getting session token...')
-        const sessionToken = step.getSessionToken()
-        TokenManager.getTokens({ forceRenew: false }).then((tokens) => {
-          console.log('ForgeRock getTokens returned', tokens)
-          console.log('ForgeRock getting current user...')
-          return Promise.all([tokens, UserManager.getCurrentUser()])
-        }).then(([tokens, user]) => {
-          console.log('ForgeRock getCurrentUser returned', user)
-          console.log('Resolving login() promise', {
-            sessionToken,
-            tokens,
-            user
-          })
-          return resolve({
-            sessionToken,
-            tokens,
-            user
-          })
+  const handleStep = (step) => {
+    if (step.type === StepType.LoginSuccess) {
+      console.log('ForgeRock login success', step)
+      console.log('ForgeRock getting session token...')
+      const sessionToken = step.getSessionToken()
+
+      TokenManager.getTokens({ forceRenew: true }).then((tokens) => {
+        console.log('ForgeRock getTokens returned', tokens)
+        return onSuccess({
+          sessionToken,
+          tokens
         })
+      })
 
-        return
-      }
+      return
+    }
 
-      if (step.type === StepType.LoginFailure) {
-        console.log('ForgeRock login failure', step)
-        return reject(step)
-      }
+    if (step.type === StepType.LoginFailure) {
+      console.log('ForgeRock login failure', step)
+      return onFailure(step)
+    }
 
-      console.log('Stepping', step)
-      // This is not a failure or success step so figure out what data the step
-      // needs and fill it, then call nextStep with it
-      const usernameCallbacks = step.getCallbacksOfType('NameCallback')
-      const passwordCallbacks = step.getCallbacksOfType('PasswordCallback')
+    console.log('Stepping', step)
+    onUpdateUi(step, (formData) => {
+      // Fill in the step input data from form data
+      step.callbacks.forEach((callback) => {
+        const payload = callback?.payload
+        const inputs = payload?.input || []
 
-      if (!usernameCallbacks.length || !passwordCallbacks.length) return console.log('Error, do not recognise step')
-
-      const nameCallback = step.getCallbackOfType('NameCallback')
-      const passwordCallback = step.getCallbackOfType('PasswordCallback')
-
-      nameCallback.setName(username)
-      passwordCallback.setPassword(password)
+        inputs.forEach((input) => {
+          input.value = formData[input.name]
+        })
+      })
 
       nextStep(step)
-    }
+    })
+  }
 
-    // Start the login process
-    nextStep()
-  })
-}
-
-export const loginFlow = () => {
-  const submitData = () => {}
-  return submitData
+  // Start the login process
+  nextStep()
 }
 
 export const getCallbackElementData = (callbackData, matchFunc) => {
