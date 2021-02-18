3973479d29346964f0eba74fc14d27354fdc0d8b
robevans <rob@irrelon.com>
2021-02-18 11:53:47 +0000

    Removed console.log

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import Footer from '../components/general-ui/Footer'
 function MyApp ({ Component, pageProps }) {
   React.useEffect(() => {
     import('govuk-frontend').then(({ initAll }) => {
-      console.log('Init govuk frontend')
       document.body.className = document.body.className ? document.body.className.replace('js-disabled', '') : ''
       document.body.className = document.body.className ? document.body.className + ' js-enabled' : 'js-enabled'
       initAll()
