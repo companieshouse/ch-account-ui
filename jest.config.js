22d45cb1c345327dba5f119985bd588b8c56ff14
robevans <rob@irrelon.com>
2021-02-24 13:01:10 +0000

    Fixed jest config interfering with builds and running

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': ['<rootDir>/node_modules/babel-jest', { configFile: './config/jest/babel.config.js' }],
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   }
 }
