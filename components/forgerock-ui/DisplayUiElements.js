90a4fc6d529ab82f17ab83799dc21968e37265b1
robevans <rob@irrelon.com>
2021-02-25 14:23:09 +0000

    Abstracted element display component to share across multiple pages

---
+++ components/forgerock-ui/DisplayUiElements.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { CallbackType } from '../../services/forgerock'
+import NameCallback from './NameCallback'
+import PasswordCallback from './PasswordCallback'
+
+const DisplayUiElements = ({ elements = [], errors = [] }) => {
+  return (
+    <>
+    {elements.map((elementPayload, index) => {
+      const element = elementPayload.payload
+      console.log('Processing ui element', element)
+      switch (element.type) {
+        case CallbackType.NameCallback:
+          return <NameCallback key={index} element={element} errors={errors} />
+
+        case CallbackType.PasswordCallback:
+          return <PasswordCallback key={index} element={element} errors={errors} />
+
+        default:
+          return null
+      }
+    })}
+    </>
+  )
+}
+
+export default DisplayUiElements
