bca654a3e4f67711c750eaf19cd8edcbcf6b4833
robevans <rob@irrelon.com>
2021-03-01 15:08:16 +0000

    Created a normalised flow handler called forgerockFlow(), will move loginFlow over to this soon

--- services/forgerock.js
+++ services/forgerock.js
@@ -4,15 +4,15 @@ import {
   FORGEROCK_CLIENT_ID,
   FORGEROCK_REALM,
   FORGEROCK_REDIRECT,
-  FORGEROCK_SCOPE,
-  FORGEROCK_TREE_LOGIN
+  FORGEROCK_SCOPE
 } from './environment'
 export { CallbackType } from '@forgerock/javascript-sdk'
 
-export const loginFlow = ({
+export const forgerockFlow = ({
   onSuccess,
   onFailure,
-  onUpdateUi
+  onUpdateUi,
+  journeyName
 }) => {
   Config.set({
     clientId: FORGEROCK_CLIENT_ID,
@@ -23,7 +23,87 @@ export const loginFlow = ({
       baseUrl: FORGEROCK_AM,
       timeout: 30000
     },
-    tree: FORGEROCK_TREE_LOGIN
+    tree: journeyName
+  })
+
+  const handleFatalError = (err) => {
+    console.log('ForgeRock fatal error', err)
+    onFailure(err)
+  }
+
+  const nextStep = (step) => {
+    console.log('ForgeRock calling next step', step)
+    FRAuth.next(step).then(handleStep).catch(handleFatalError)
+  }
+
+  const handleStep = (step) => {
+    console.log('Forgerock step, handleStep(step) got', step)
+    if (step.type === StepType.LoginSuccess) {
+      console.log('ForgeRock login success', step)
+      console.log('ForgeRock getting session token...')
+      const sessionToken = step.getSessionToken()
+
+      TokenManager.getTokens({ forceRenew: true }).then((tokens) => {
+        return Promise.all([tokens, UserManager.getCurrentUser()])
+      }).then(([tokens, currentUser]) => {
+        console.log('ForgeRock getTokens returned', tokens)
+        console.log('Resolving promise with', {
+          sessionToken,
+          tokens,
+          currentUser
+        })
+
+        return onSuccess({
+          sessionToken,
+          tokens,
+          currentUser
+        })
+      })
+
+      return
+    }
+
+    if (step.type === StepType.LoginFailure) {
+      console.log('ForgeRock login failure', step)
+      return onFailure(step)
+    }
+
+    console.log('Stepping', step)
+    onUpdateUi(step, (formData) => {
+      // Fill in the step input data from form data
+      step.callbacks.forEach((callback) => {
+        const payload = callback?.payload
+        const inputs = payload?.input || []
+
+        inputs.forEach((input) => {
+          input.value = formData[input.name]
+        })
+      })
+
+      nextStep(step)
+    })
+  }
+
+  // Start the login process
+  nextStep()
+}
+
+export const loginFlow = ({
+  onSuccess,
+  onFailure,
+  onUpdateUi,
+  journeyName
+}) => {
+  Config.set({
+    clientId: FORGEROCK_CLIENT_ID,
+    realmPath: FORGEROCK_REALM,
+    redirectUri: FORGEROCK_REDIRECT,
+    scope: FORGEROCK_SCOPE,
+    serverConfig: {
+      baseUrl: FORGEROCK_AM,
+      timeout: 30000
+    },
+    tree: journeyName
   })
 
   const handleFatalError = (err) => {
@@ -105,17 +185,23 @@ export const logoutFlow = ({
   SessionManager.logout().then(onSuccess).catch(onFailure)
 }
 
-export const getCallbackElementData = (callbackData, matchFunc) => {
+export const getCallbackElementData = (callbackData) => {
   const { input, output } = callbackData
 
-  for (let i = 0; i < output.length; i++) {
-    if (matchFunc(output[i], i) === true) {
-      return {
-        input: input[i],
-        output: output[i]
-      }
-    }
-  }
+  // Find the field id
+  const fieldId = input[0].name
+  const label = output.find((outputItem) => outputItem.name === 'prompt')?.value || ''
+  const value = output.find((outputItem) => outputItem.name === 'value')?.value || ''
+  const required = output.find((outputItem) => outputItem.name === 'required')?.value || false
+  const testId = output.find((outputItem) => outputItem.name === 'name')?.value || 'unknownFieldName'
 
-  return null
+  return {
+    fieldId,
+    label,
+    value,
+    required,
+    testId,
+    input,
+    output
+  }
 }
