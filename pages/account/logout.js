a8ffd1922f6392b9bcc4b9e9893f6f526f628e16
robevans <rob@irrelon.com>
2021-03-02 10:17:09 +0000

    Moved the logout view to the page file

--- pages/account/logout.js
+++ pages/account/logout.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import Router from 'next/router'
-import LogoutView from '../../components/views/account/LogoutView'
 import { logoutFlow } from '../../services/forgerock'
 import HeadingCount from '../../services/HeadingCount'
+import Main from '../../components/general-ui/layout/Main'
+import Row from '../../components/general-ui/layout/Row'
+import Column from '../../components/general-ui/layout/Column'
+import PageHeading from '../../components/general-ui/typeography/PageHeading'
+import BodyText from '../../components/general-ui/typeography/BodyText'
+import LinkText from '../../components/general-ui/interaction/LinkText'
+import WidthContainer from '../../components/general-ui/layout/WidthContainer'
 
 const Logout = () => {
   const [errors, setErrors] = React.useState([])
@@ -11,7 +17,7 @@ const Logout = () => {
 
   const doLogout = () => {
     logoutFlow({
-      onSuccess: (loginData) => {
+      onSuccess: () => {
         Router.push('/account/login')
       },
       onFailure: (err) => {
@@ -30,7 +36,18 @@ const Logout = () => {
   }, [])
 
   return (
-    <LogoutView errors={errors} headingCount={headingCount} />
+    <WidthContainer>
+      <Main>
+        <Row>
+          <Column width='two-thirds'>
+            <PageHeading headingCount={headingCount} errors={errors}>Sign out of your Companies House account</PageHeading>
+            <BodyText>
+              <LinkText href={'/account/login'} testId='loginToAccountLink'>Sign in to your account</LinkText>
+            </BodyText>
+          </Column>
+        </Row>
+      </Main>
+    </WidthContainer>
   )
 }
 
