1d67d672e5cfdf59086dac25a3cf819f58b185f5
robevans <rob@irrelon.com>
2021-03-08 14:23:52 +0000

    Added reset password journey to environment vars

--- services/environment.js
+++ services/environment.js
@@ -1,6 +1,6 @@
 // These settings default to dev environment values if no value is present
 export const ENVIRONMENT = process.env.ENVIRONMENT || 'dev'
-export const FORGEROCK_AM = process.env.FORGEROCK_AM || 'https://openam-amido-demo.forgeblocks.com/am/'
+export const FORGEROCK_AM = process.env.FORGEROCK_AM || 'https://openam-companieshouse-uk-dev.id.forgerock.io/am/'
 export const FORGEROCK_REALM = process.env.FORGEROCK_REALM || '/alpha'
 export const FORGEROCK_SCOPE = process.env.FORGEROCK_SCOPE || 'openid fr:idm:*'
 export const FORGEROCK_CLIENT_ID = process.env.FORGEROCK_CLIENT_ID || 'ForgeRockSDKClient'
@@ -8,5 +8,6 @@ export const FORGEROCK_REDIRECT = process.env.FORGEROCK_REDIRECT || 'http://loca
 
 export const FORGEROCK_TREE_LOGIN = process.env.FORGEROCK_TREE_LOGIN || 'Login'
 export const FORGEROCK_TREE_REGISTER = process.env.FORGEROCK_TREE_REGISTER || 'CHRegistration'
+export const FORGEROCK_TREE_FMP = process.env.FORGEROCK_TREE_FMP || 'CHResetPassword'
 
 console.log(`Running as environment: ${ENVIRONMENT}`)
