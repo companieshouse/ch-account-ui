24c2cbaef72fcb4feec127745c1187ba19aa3c4c
robevans <rob@irrelon.com>
2021-03-15 18:13:33 +0000

    Split the language data into features and tokens

---
+++ services/lang/withLang.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useCookies } from 'react-cookie'
+
+const withLang = (WrappedComponent) => (props) => {
+  const [cookies, setCookie] = useCookies(['lang'])
+  const { lang = 'en' } = cookies
+
+  return <WrappedComponent {...props} lang={lang} setLang={(newLang) => setCookie('lang', newLang)} />
+}
+
+export default withLang
