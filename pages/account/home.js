40aad543b2009c5fc5b9f126f327dde78d5d70d7
robevans <rob@irrelon.com>
2021-03-15 18:21:51 +0000

    Some minor work on the account/home page to update to latest page changes across the app

--- pages/account/home.js
+++ pages/account/home.js
@@ -15,13 +15,44 @@ import LinkText from '../../components/general-ui/interaction/LinkText'
 import CompanySummary from '../../components/application-specific/CompanySummary'
 import Button from '../../components/general-ui/interaction/Button'
 import WidthContainer from '../../components/general-ui/layout/WidthContainer'
+import { useRouter } from 'next/router'
+import UiFeatures from '../../components/general-ui/UiFeatures'
+import { getStageFeatures, translate } from '../../services/translate'
+import withLang from '../../services/lang/withLang'
 
-const Home = ({ errors, companies, userDetails }) => {
+const Home = ({ errors, lang }) => {
+  const uiStage = 'HOME_OVERVIEW'
   const headingCount = new HeadingCount()
+
+  const [userDetails, setUserDetails] = React.useState({
+    fullName: 'Test User',
+    emailAddress: 'test@user.com'
+  })
+
+  const [companies, setCompanies] = React.useState([{
+    name: 'Test Company',
+    number: '0123456789',
+    address: '2nd Floor Red House, 17 London Road, London, SA73 8PH',
+    personsAuthorisedToFile: [{
+      name: 'Test User',
+      emailAddress: 'test@user.com',
+      permissions: ['FILE_ACCOUNTS', 'FILE_CONFIRMATION_STATEMENTS', 'MAKE_CHANGES_TO_THE_COMPANY'],
+      canAuthOthers: true,
+      status: 'Confirmed'
+    }]
+  }])
+
+  const router = useRouter()
+  const { notifyType, notifyHeading, notifyTitle, notifyChildren } = router.query
+  const uiFeatures = getStageFeatures('en', uiStage)
 
   React.useEffect(() => {
     headingCount.reset()
   }, [])
+
+  const renderFeatures = (props) => {
+    return <UiFeatures {...props} />
+  }
 
   return (
     <WidthContainer>
@@ -29,7 +60,7 @@ const Home = ({ errors, companies, userDetails }) => {
       <Main>
         <Row>
           <Column width='full'>
-            {errors.length === 0 && <HeadingText headingCount={headingCount} size="l" caption='Home'>{userDetails.fullName}</HeadingText>}
+            {errors.length === 0 && <HeadingText headingCount={headingCount} size="l" caption={translate(lang, 'home')}>{userDetails.fullName}</HeadingText>}
             {errors.length > 0 && <>
               <ErrorSummary headingCount={headingCount} title="There is a problem" errors={errors}/>
               <HeadingText headingCount={headingCount} size="l" caption='Home'>{userDetails.fullName}</HeadingText>
@@ -63,30 +94,7 @@ const Home = ({ errors, companies, userDetails }) => {
   )
 }
 
-Home.getInitialProps = async (context) => {
-  return {
-
-    errors: [],
-    userDetails: {
-      fullName: 'Test User',
-      emailAddress: 'test@user.com'
-    },
-    companies: [{
-      name: 'Test Company',
-      number: '0123456789',
-      address: '2nd Floor Red House, 17 London Road, London, SA73 8PH',
-      personsAuthorisedToFile: [{
-        name: 'Test User',
-        emailAddress: 'test@user.com',
-        permissions: ['FILE_ACCOUNTS', 'FILE_CONFIRMATION_STATEMENTS', 'MAKE_CHANGES_TO_THE_COMPANY'],
-        canAuthOthers: true,
-        status: 'Confirmed'
-      }]
-    }]
-  }
-}
-
-export default Home
+export default withLang(Home)
 
 Home.propTypes = {
   companies: PropTypes.array,
