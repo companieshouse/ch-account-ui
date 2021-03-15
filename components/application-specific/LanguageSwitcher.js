4c122ee93bd74dfce76a953def76cb28c3d2fd9b
robevans <rob@irrelon.com>
2021-03-15 19:38:53 +0000

    Fixed some css classname erros in language switcher

--- components/application-specific/LanguageSwitcher.js
+++ components/application-specific/LanguageSwitcher.js
@@ -9,18 +9,18 @@ const LanguageSwitcher = ({ lang, setLang }) => {
         {lang === 'en' && <li className="govuk-language-select__list-item">
           <span aria-current="true">English</span>
         </li>}
-        {lang !== 'en' && <li className="hmrc-language-select__list-item">
-          <a href="?lang=en" hrefLang="en" lang="en" rel="alternate" className="govuk-link"
+        {lang !== 'en' && <li className="govuk-language-select__list-item">
+          <a href="#lang-en" hrefLang="en" lang="en" rel="alternate" className="govuk-link"
              data-journey-click="link - click:lang-select:English" onClick={() => setLang('en')}>
             <span className="govuk-visually-hidden">Change the language to English</span>
             <span aria-hidden="true">English</span>
           </a>
         </li>}
-        {lang === 'cy' && <li className="hmrc-language-select__list-item">
+        {lang === 'cy' && <li className="govuk-language-select__list-item">
           <span aria-current="true">Cymraeg</span>
         </li>}
         {lang !== 'cy' && <li className="govuk-language-select__list-item">
-          <a href="?lang=cy" hrefLang="cy" lang="cy" rel="alternate" className="govuk-link"
+          <a href="#lang-cy" hrefLang="cy" lang="cy" rel="alternate" className="govuk-link"
              data-journey-click="link - click:lang-select:Cymraeg" onClick={() => setLang('cy')}>
             <span className="govuk-visually-hidden">Newid yr iaith ir Gymraeg</span>
             <span aria-hidden="true">Cymraeg</span>
