788b05dff9fea31d45c3c593bdceec2722d35f68
robevans <rob@irrelon.com>
2021-03-24 10:46:23 +0000

    Added SummaryList.js component

---
+++ components/general-ui/typeography/SummaryList.js
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import NlToBr from '../layout/NlToBr'
+
+const SummaryList = (props) => {
+  const { children, className = '', renderFeatures, listItems = [] } = props
+  const classes = [className]
+
+  const finalClassName = classes.join(' ').trim()
+
+  return (
+    <dl className={`govuk-summary-list ${finalClassName}`}>
+      {listItems.map((listItem, index) => <div key={`${listItem.label}_${index}`} className="govuk-summary-list__row">
+        <dt className="govuk-summary-list__key">
+          <NlToBr content={listItem.label} />
+        </dt>
+        <dd className="govuk-summary-list__value">
+          <NlToBr content={listItem.value} />
+        </dd>
+        {listItem.action && <dd className="govuk-summary-list__actions">
+          <a className="govuk-link" href={`${listItem.action.href}`}>
+            <NlToBr content={listItem.action.label} /><span className="govuk-visually-hidden"> {listItem.action.desc || ''}</span>
+          </a>
+        </dd>}
+      </div>)}
+      {children}
+      {renderFeatures(props)}
+    </dl>
+  )
+}
+
+export default SummaryList
+
+SummaryList.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  renderFeatures: PropTypes.func,
+  listItems: PropTypes.array
+}
+
+SummaryList.defaultProps = {
+  className: '',
+  renderFeatures: () => { return null },
+  listItems: []
+}
