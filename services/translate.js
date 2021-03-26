0d2790f3056d812710e3363d31d95fddb2238730
robevans <rob@irrelon.com>
2021-03-26 09:12:47 +0000

    Debug page stages by allowing the DisplayUiElements feature to output the forgerock element data regardless of if we have a matching stage in the translation files or not

--- services/translate.js
+++ services/translate.js
@@ -19,6 +19,8 @@ export const getStageFeatures = (lang = 'en', stage = '', featureName = '') => {
     return [{
       feature: 'BodyText',
       children: `Cannot find features for lang "${lang}".`
+    }, {
+      feature: 'DisplayUiElements'
     }]
   }
 
@@ -26,6 +28,8 @@ export const getStageFeatures = (lang = 'en', stage = '', featureName = '') => {
     return [{
       feature: 'BodyText',
       children: `Cannot find stage data for lang "${lang}" and stage "${stage}".  Either the journey page node has not been given a stage name or you are not correctly passing the stage name to the getStageFeatures(lang, stage, featureName) function!`
+    }, {
+      feature: 'DisplayUiElements'
     }]
   }
 
@@ -37,6 +41,8 @@ export const getStageFeatures = (lang = 'en', stage = '', featureName = '') => {
     return [{
       feature: 'BodyText',
       children: `No feature data for lang "${lang}", stage "${stage}" and featureName "${featureName}". Please check your stage data to ensure you have defined a feature with this name!`
+    }, {
+      feature: 'DisplayUiElements'
     }]
   }
 
