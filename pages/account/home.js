c2d77a58e27a9a22a00a0105d294c8fc4792db1c
robevans <rob@irrelon.com>
2021-03-29 17:43:41 +0100

    Home page redesign

--- pages/account/home.js
+++ pages/account/home.js
@@ -4,11 +4,16 @@ import HeadingCount from '../../services/HeadingCount'
 import { useRouter } from 'next/router'
 import UiFeatures from '../../components/general-ui/UiFeatures'
 import withLang from '../../services/lang/withLang'
+import FeatureDynamicView from '../../components/views/FeatureDynamicView'
+import { getStageFeatures } from '../../services/translate'
 
 const Home = ({ errors, lang }) => {
   const uiStage = 'HOME_OVERVIEW'
   const headingCount = new HeadingCount()
 
+  const [customPageProps, setCustomPageProps] = React.useState({})
+  const [uiFeatures, setUiFeatures] = React.useState(getStageFeatures(lang, uiStage))
+  const [uiElements, setUiElements] = React.useState([])
   const [userDetails, setUserDetails] = React.useState({
     fullName: 'Test User',
     emailAddress: 'test@user.com'
@@ -34,43 +39,26 @@ const Home = ({ errors, lang }) => {
     headingCount.reset()
   }, [notifyType, notifyHeading, notifyTitle, notifyChildren])
 
+  const renderFeatures = (props) => {
+    return <UiFeatures {...props} />
+  }
+
   return (
-    <WidthContainer>
-      <AccountLinks userDetails={userDetails} />
-      <Main>
-        <Row>
-          <Column width='full'>
-            {errors.length === 0 && <HeadingText headingCount={headingCount} size="l" caption={translate(lang, 'LABEL_HOME')}>{userDetails.fullName}</HeadingText>}
-            {errors.length > 0 && <>
-              <ErrorSummary headingCount={headingCount} title="There is a problem" errors={errors}/>
-              <HeadingText headingCount={headingCount} size="l" caption='Home'>{userDetails.fullName}</HeadingText>
-            </>}
-
-            <HeadingText headingCount={headingCount} size="s">Your companies</HeadingText>
-            <SectionBreak />
-
-            {companies.length === 0 && <>
-              <HeadingText headingCount={headingCount} size="m">You have not added any companies to this account</HeadingText>
-              <BodyText>
-                To file online for a company, you must add it to your account.
-              </BodyText>
-              <BodyText>
-                You&apos;ll need:
-              </BodyText>
-              <List>
-                <ListItem>the company number</ListItem>
-                <ListItem>the company <LinkText href="https://www.gov.uk/guidance/company-authentication-codes-for-online-filing" testId="authenticationCodeLink">authentication code</LinkText></ListItem>
-              </List>
-            </>}
-            {companies.map((company) => <CompanySummary key={company} company={company} headingCount={headingCount} />)}
-            <SectionBreak />
-            <Button type="link" className="govuk-button" data-module="govuk-button" testId="addAnotherCompanyButton">
-              Add another company
-            </Button>
-          </Column>
-        </Row>
-      </Main>
-    </WidthContainer>
+    <FeatureDynamicView
+      width="full"
+      hasBackLink={false}
+      renderFeatures={renderFeatures}
+      errors={errors}
+      headingCount={headingCount}
+      uiFeatures={uiFeatures}
+      uiElements={uiElements}
+      uiStage={uiStage}
+      notifyType={notifyType}
+      notifyHeading={notifyHeading}
+      notifyTitle={notifyTitle}
+      notifyChildren={notifyChildren}
+      {...customPageProps}
+    />
   )
 }
 
