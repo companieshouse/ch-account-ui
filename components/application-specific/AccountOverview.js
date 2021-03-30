ad8a3b0f54f791e52eba5089ae17520208460773
robevans <rob@irrelon.com>
2021-03-30 09:02:03 +0100

    Fixed typos

--- components/application-specific/AccountOverview.js
+++ components/application-specific/AccountOverview.js
@@ -8,17 +8,17 @@ const AccountOverview = () => {
       <div className="govuk-grid-column-one-third block">
         <HeadingText type="h2" size="m">0</HeadingText>
         <HeadingText type="h3" size="m">Your companies</HeadingText>
-        <BodyText>View and manage your companies and the people authorised top file n behalf of a company.</BodyText>
+        <BodyText>View and manage your companies and the people authorised to file on behalf of a company.</BodyText>
       </div>
       <div className="govuk-grid-column-one-third block">
         <HeadingText type="h2" size="m">0</HeadingText>
-        <HeadingText type="h3" size="m">Your companies</HeadingText>
-        <BodyText>View and manage your companies and the people authorised top file n behalf of a company.</BodyText>
+        <HeadingText type="h3" size="m">Companies you follow</HeadingText>
+        <BodyText>To receive notifications about a company, search for a company on the Companies House register and use the &apos;Follow a company&apos; service.</BodyText>
       </div>
       <div className="govuk-grid-column-one-third block">
         <HeadingText type="h2" size="m">0</HeadingText>
-        <HeadingText type="h3" size="m">Your companies</HeadingText>
-        <BodyText>View and manage your companies and the people authorised top file n behalf of a company.</BodyText>
+        <HeadingText type="h3" size="m">Notifications</HeadingText>
+        <BodyText>Requests for authorised people to file on behalf of a company will appear here.</BodyText>
       </div>
       <style jsx>{`
         .block {
