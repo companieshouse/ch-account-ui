4274a0bc0da8e95f3e723404871656a055b5dfa3
robevans <rob@irrelon.com>
2021-04-12 12:41:00 +0100

    Added onClick prop to LinkText so we can allow JS-based onClick handlers (to support show/hide password on password fields)

--- components/general-ui/interaction/LinkText.js
+++ components/general-ui/interaction/LinkText.js
@@ -3,14 +3,14 @@ import React from 'react'
 import Link from 'next/link'
 
 const LinkText = (props) => {
-  const { children, href, className = '', testId, renderFeatures } = props
+  const { children, href, className = '', testId, renderFeatures, onClick } = props
   const classes = [className]
   const finalClassName = classes.join(' ').trim()
 
   return (
     <Link href={href}>
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a className={`govuk-link ${finalClassName}`} data-testid={testId}>{children}{renderFeatures(props)}</a>
+      <a onClick={onClick} className={`govuk-link ${finalClassName}`} data-testid={testId}>{children}{renderFeatures(props)}</a>
     </Link>
   )
 }
@@ -22,7 +22,8 @@ LinkText.propTypes = {
   className: PropTypes.string,
   href: PropTypes.string.isRequired,
   testId: PropTypes.string.isRequired,
-  renderFeatures: PropTypes.func
+  renderFeatures: PropTypes.func,
+  onClick: PropTypes.func
 }
 
 LinkText.defaultProps = {
