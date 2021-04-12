1d33d2e7b5e8b34dc968bd66b15166a953b2ef9b
robevans <rob@irrelon.com>
2021-04-12 15:38:17 +0100

    Account management change password handler page

---
+++ pages/account/manage/change-password/[pageStep].js
@@ -0,0 +1,147 @@
+import React from 'react'
+import HeadingCount from '../../../../services/HeadingCount'
+import { findCustomPageProps, findCustomStage, forgerockFlow } from '../../../../services/forgerock'
+import { FORGEROCK_TREE_CHANGE_PASSWORD } from '../../../../services/environment'
+import Router, { useRouter } from 'next/router'
+import { getStageFeatures, translate } from '../../../../services/translate'
+import UiFeatures from '../../../../components/general-ui/UiFeatures'
+import FeatureDynamicView from '../../../../components/views/FeatureDynamicView'
+import withLang from '../../../../services/lang/withLang'
+import componentMap from '../../../../services/componentMap'
+import Dynamic from '../../../../components/Dynamic'
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { pageStep: '_start' } },
+      { params: { pageStep: '_restart' } }
+    ],
+    fallback: false
+  }
+}
+
+export const getStaticProps = async () => {
+  return { props: {} }
+}
+
+const RequestAuthCode = ({ lang }) => {
+  const router = useRouter()
+  const [errors, setErrors] = React.useState([])
+  const [customPageProps, setCustomPageProps] = React.useState({})
+  const [uiStage, setUiStage] = React.useState('')
+  const [uiFeatures, setUiFeatures] = React.useState([])
+  const [uiElements, setUiElements] = React.useState([])
+  const [submitData, setSubmitData] = React.useState((formData) => {})
+  const headingCount = new HeadingCount()
+
+  const { pageStep = '', service = '', token, overrideStage = '' } = router.query
+  const { notifyType, notifyHeading, notifyTitle, notifyChildren } = router.query
+
+  let journeyName = ''
+
+  React.useEffect(() => {
+    headingCount.reset()
+    if (!pageStep) return
+
+    if (pageStep === '_restart') {
+      router.replace('/account/manage/change-password/_start/')
+      return
+    }
+
+    journeyName = FORGEROCK_TREE_CHANGE_PASSWORD
+
+    setErrors([])
+
+    const stepOptions = {
+      query: {
+        token
+      }
+    }
+
+    console.log('Staring FR journey', journeyName, stepOptions)
+    forgerockFlow({
+      journeyName,
+      journeyNamespace: 'CHANGE_PASSWORD',
+      stepOptions,
+      onSuccess: () => {
+        Router.push('/account/manage')
+      },
+      onFailure: (errData, newErrors = []) => {
+        // We only get here if there was a fatal error signal from the forgerock client library
+        // all other errors are not considered a failure (such as incorrectly formatted inputs etc
+        // and are handled gracefully by the onUpdateUi function
+        setErrors(newErrors)
+        setUiFeatures(getStageFeatures(lang, overrideStage || 'CHANGE_PASSWORD_1'))
+      },
+      onUpdateUi: (step, submitDataFunc, stepErrors = []) => {
+        const stepCustomPageProps = findCustomPageProps(step)
+        const stage = step.payload.stage || findCustomStage(step)
+        step.payload.stage = stage
+
+        if (stepCustomPageProps) {
+          if (stepCustomPageProps.apiError) {
+            // Transform the apiError structure to the app's errors array structure
+            const apiErrorsAsAppErrors = stepCustomPageProps.apiError.errors.map((errorItem) => ({
+              label: errorItem.message
+            }))
+
+            stepErrors.push(...apiErrorsAsAppErrors)
+          }
+        }
+
+        // Update the errors for the page
+        setErrors((currentErrorsArray) => {
+          return [...currentErrorsArray, ...stepErrors]
+        })
+
+        setCustomPageProps(stepCustomPageProps)
+        setUiStage(stage)
+        setUiFeatures(getStageFeatures(lang, overrideStage || stage))
+        setUiElements(step.callbacks)
+        setSubmitData(() => submitDataFunc)
+      }
+    })
+  }, [pageStep, overrideStage, service, token])
+
+  const onSubmit = (evt) => {
+    evt.preventDefault()
+    setErrors([])
+
+    // Convert UI element values to JSON key/value pairs
+    const formData = Object.entries(evt.target.elements).reduce((obj, [key, element]) => {
+      obj[key] = element.value
+      return obj
+    }, {})
+
+    submitData(formData)
+  }
+
+  // Check if the router has been initialised yet
+  if (!pageStep) return null
+
+  return (
+    <FeatureDynamicView
+      width='two-thirds'
+      onSubmit={onSubmit}
+      errors={errors}
+      headingCount={headingCount}
+      uiStage={uiStage}
+      notifyType={notifyType}
+      notifyHeading={notifyHeading}
+      notifyTitle={notifyTitle}
+      notifyChildren={notifyChildren}
+      {...customPageProps}
+    >
+      <Dynamic
+        componentMap={componentMap}
+        headingCount={headingCount}
+        content={uiFeatures}
+        errors={errors}
+        uiElements={uiElements}
+        uiStage={uiStage}
+      />
+    </FeatureDynamicView>
+  )
+}
+
+export default withLang(RequestAuthCode)
