9145b5ed393fc9d96d8344c15e333e4bd4a06e94
robevans <rob@irrelon.com>
2021-04-12 15:41:35 +0100

    Added componentMap which will be the new way to add "features"

---
+++ services/componentMap.js
@@ -0,0 +1,73 @@
+import Button from '../components/general-ui/interaction/Button'
+import HeadingText from '../components/general-ui/typeography/HeadingText'
+import BodyText from '../components/general-ui/typeography/BodyText'
+import SpanText from '../components/general-ui/typeography/SpanText'
+import LinkText from '../components/general-ui/interaction/LinkText'
+import DisplayUiElements from '../components/forgerock-ui/DisplayUiElements'
+import PageHeading from '../components/general-ui/typeography/PageHeading'
+import List from '../components/general-ui/typeography/List'
+import InsetText from '../components/general-ui/typeography/InsetText'
+import Details from '../components/general-ui/typeography/Details'
+import TemplateText from '../components/general-ui/typeography/TemplateText'
+import NotificationBanner from '../components/general-ui/typeography/NotificationBanner'
+import RadioGroup from '../components/general-ui/interaction/RadioGroup'
+import Redirect from '../components/application-specific/Redirect'
+import BrowserTitle from '../components/application-specific/BrowserTitle'
+import Row from '../components/general-ui/layout/Row'
+import Column from '../components/general-ui/layout/Column'
+import Main from '../components/general-ui/layout/Main'
+import Table from '../components/general-ui/layout/Table'
+import TBody from '../components/general-ui/layout/TBody'
+import Td from '../components/general-ui/layout/Td'
+import Th from '../components/general-ui/layout/Th'
+import THead from '../components/general-ui/layout/THead'
+import Tr from '../components/general-ui/layout/Tr'
+import WidthContainer from '../components/general-ui/layout/WidthContainer'
+import SummaryList from '../components/general-ui/typeography/SummaryList'
+import NlToBr from '../components/general-ui/layout/NlToBr'
+import Fragment from '../components/general-ui/typeography/Fragment'
+import Caption from '../components/general-ui/typeography/Caption'
+import SectionBreak from '../components/general-ui/typeography/SectionBreak'
+import InfoBlocks from '../components/application-specific/InfoBlocks'
+import InfoBlock from '../components/application-specific/InfoBlock'
+import AccountLinks from '../components/application-specific/AccountLinks'
+import SummaryListItem from '../components/general-ui/typeography/SummaryListItem'
+
+const componentMap = {
+  Button,
+  HeadingText,
+  BodyText,
+  SpanText,
+  LinkText,
+  DisplayUiElements,
+  PageHeading,
+  List,
+  InsetText,
+  Details,
+  TemplateText,
+  NotificationBanner,
+  RadioGroup,
+  Redirect,
+  BrowserTitle,
+  Row,
+  Column,
+  Main,
+  Table,
+  TBody,
+  Td,
+  Th,
+  THead,
+  Tr,
+  WidthContainer,
+  SummaryList,
+  NlToBr,
+  Fragment,
+  Caption,
+  SectionBreak,
+  InfoBlocks,
+  InfoBlock,
+  AccountLinks,
+  SummaryListItem
+}
+
+export default componentMap
