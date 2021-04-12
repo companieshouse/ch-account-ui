41af72b3a3c2331bcbc526998a222d270ce28bf2
robevans <rob@irrelon.com>
2021-04-12 11:28:49 +0100

    Added show/hide password functionality to PasswordCallback.js

--- components/forgerock-ui/PasswordCallback.js
+++ components/forgerock-ui/PasswordCallback.js
@@ -3,27 +3,56 @@ import React from 'react'
 import FormGroup from '../general-ui/interaction/FormGroup'
 import InputField from '../general-ui/interaction/InputField'
 import { errorsPropType } from '../../services/propTypes'
+import LinkText from '../general-ui/interaction/LinkText'
+import { translate } from '../../services/translate'
+import withLang from '../../services/lang/withLang'
 
-const PasswordCallback = ({ errors = [], element, customElementProps = {} }) => {
+const PasswordCallback = ({ lang, errors = [], element, customElementProps = {}, hasShowPasswordSuffix = true }) => {
+  const [showPassword, setShowPassword] = React.useState(false)
   const id = element.payload.input[0].name
   const label = customElementProps.prompt || element.getPrompt()
 
   return (
     <FormGroup errors={errors} groupIds={[id]}>
-      <InputField id={id} type="password" autoComplete="current-password" label={label} errors={errors} testId="passwordInputField" {...customElementProps} />
+      <InputField
+        id={id}
+        type={showPassword ? 'text' : 'password'}
+        autoComplete="current-password"
+        label={label}
+        errors={errors}
+        testId="passwordInputField"
+        suffix={hasShowPasswordSuffix === true && <LinkText
+          testId={`showHidePassword_${id}`}
+          href={'#showHidePassword'}
+          onClick={(ev) => {
+            ev.preventDefault()
+            ev.stopPropagation()
+
+            setShowPassword(!showPassword)
+
+            return false
+          }}
+        >
+          {translate(lang, `PASSWORD_${showPassword ? 'HIDE' : 'SHOW'}`)}
+        </LinkText>}
+        {...customElementProps}
+      />
     </FormGroup>
   )
 }
 
-export default PasswordCallback
+export default withLang(PasswordCallback)
 
 PasswordCallback.propTypes = {
   customElementProps: PropTypes.object,
   element: PropTypes.object.isRequired,
+  hasShowPasswordSuffix: PropTypes.bool,
+  lang: PropTypes.string.isRequired,
   errors: errorsPropType
 }
 
 PasswordCallback.defaultProps = {
   customElementProps: {},
-  errors: []
+  errors: [],
+  hasShowPasswordSuffix: true
 }
