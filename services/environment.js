2956886733a703a20a8e82749aedab3b518faa61
robevans <rob@irrelon.com>
2021-04-13 13:26:31 +0100

    Added new scopes to get more user data back in token

--- services/environment.js
+++ services/environment.js
@@ -2,7 +2,7 @@
 export const ENVIRONMENT = process.env.ENVIRONMENT || 'dev'
 export const FORGEROCK_AM = process.env.FORGEROCK_AM || 'https://openam-companieshouse-uk-dev.id.forgerock.io/am/'
 export const FORGEROCK_REALM = process.env.FORGEROCK_REALM || '/alpha'
-export const FORGEROCK_SCOPE = process.env.FORGEROCK_SCOPE || 'openid fr:idm:*'
+export const FORGEROCK_SCOPE = process.env.FORGEROCK_SCOPE || 'openid email phone profile fr:idm:*'
 export const FORGEROCK_CLIENT_ID = process.env.FORGEROCK_CLIENT_ID || 'ForgeRockSDKClient'
 export const FORGEROCK_REDIRECT = process.env.FORGEROCK_REDIRECT || 'http://localhost:3000/account/home/'
 
