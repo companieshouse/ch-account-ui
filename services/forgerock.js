746532a9d6123f7859adc0aef3e6c6ecf4376f01
Stuart Parr <stuart.parr@amido.com>
2021-04-30 11:45:45 +0100

    Update company and user endpoints inline with switch to fr orgz

--- services/forgerock.js
+++ services/forgerock.js
@@ -252,7 +252,7 @@ export const getUsersAssociatedWithCompany = async (accessToken, companyId) => {
     return
   }
 
-  const url = `${FORGEROCK_COMPANY_ENDPOINT}${companyId}/authorisedUsers?_fields=&_queryFilter=true`
+  const url = `${FORGEROCK_COMPANY_ENDPOINT}openidm/managed/alpha_organization/${companyId}/members?_queryFilter=true&_fields=userName,givenName`
 
   const res = await fetch(url, {
     headers: {
@@ -292,8 +292,7 @@ export const getCompaniesAssociatedWithUser = async (accessToken, userId) => {
     console.error('getCompaniesAssociatedWithUser(accessToken, userId): No userId provided!')
     return
   }
-
-  const url = `${FORGEROCK_USER_ENDPOINT}${userId}/isAuthorisedUserOf?_fields=&_queryFilter=true`
+  const url = `${FORGEROCK_USER_ENDPOINT}openidm/managed/alpha_user/${userId}/memberOfOrg?_queryFilter=true`
 
   const res = await fetch(url, {
     headers: {
