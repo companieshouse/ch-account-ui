aa6f75223d6a39fef51699d3974835f3e8e58319
Stuart Parr <stuart.parr@amido.com>
2021-05-14 11:45:55 +0100

    Adding authorise user journey

---
+++ pages/account/authorise/[pageStep].js
@@ -0,0 +1,137 @@
+import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+import { FORGEROCK_TREE_INVITE_USER } from '../../../services/environment'
+import { findCustomPageProps, findCustomStage, forgerockFlow } from '../../../services/forgerock'
+import Router, { useRouter } from 'next/router'
+import { getStageFeatures } from '../../../services/translate'
+import FeatureDynamicView from '../../../components/views/FeatureDynamicView'
+import Dynamic from '../../../components/Dynamic'
+import componentMap from '../../../services/componentMap'
+import withLang from '../../../services/lang/withLang'
+import HeadingCount from '../../../services/HeadingCount'
+import { serializeForm } from '../../../services/formData'
+import withAccessToken from '../../../services/withAccessToken'
+import { generateQueryUrl } from '../../../services/queryString'
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { pageStep: '_start' } }
+    ],
+    fallback: false
+  }
+}
+
+export const getStaticProps = async () => {
+  return { props: {} }
+}
+
+const InviteUser = ({ lang }) => {
+  const router = useRouter()
+  const [uiFeatures, setUiFeatures] = useState([])
+  const [uiElements, setUiElements] = useState([])
+  const [errors, setErrors] = useState([])
+  const [uiStage, setUiStage] = useState('')
+  const [customPageProps, setCustomPageProps] = useState({})
+  const [submitData, setSubmitData] = useState((formData, stepOptions) => {})
+  const { pageStep = '', service = '', token, overrideStage = '', companyNumber } = router.query
+  const headingCount = new HeadingCount()
+
+  const getStepOptions = () => ({
+    query: { companyNumber: companyNumber, token }
+  })
+
+  React.useEffect(() => {
+    headingCount.reset()
+
+    const journeyName = FORGEROCK_TREE_INVITE_USER
+    const stepOptions = getStepOptions()
+
+    console.log(`Staring FR with pageStep "${pageStep}", journey "${journeyName}", stepOptions:`, stepOptions)
+
+    forgerockFlow({
+      journeyName,
+      journeyNamespace: 'INVITE_USER',
+      lang,
+      stepOptions,
+      onSuccess: () => {
+        Router.push('/account/your-companies')
+      },
+      onFailure: (errData, newErrors = []) => {
+        // Fatal error
+        setErrors(newErrors)
+        setUiFeatures(getStageFeatures(lang, overrideStage || 'NO_SESSION_ERROR'))
+      },
+      onUpdateUi: (step, submitDataFunc, stepErrors = []) => {
+        const stepCustomPageProps = findCustomPageProps(step)
+        const stage = step.payload.stage || findCustomStage(step)
+        step.payload.stage = stage
+
+        // Setup success URL for step 2 redirect
+        if (stage === 'INVITE_USER_2') {
+          stepCustomPageProps.authoriseSuccessPath = generateQueryUrl('/account/your-companies/', {
+            notifyToken: 'authSuccess',
+            invitedUser: stepCustomPageProps.invitedUser,
+            companyName: stepCustomPageProps.company.name
+          })
+        }
+
+        if (stepCustomPageProps?.apiError) {
+          // Transform the apiError structure to the app's errors array structure
+          const apiErrorsAsAppErrors = stepCustomPageProps.apiError.errors.map((errorItem) => ({
+            label: errorItem.message
+          }))
+
+          stepErrors.push(...apiErrorsAsAppErrors)
+        }
+
+        // // Update the errors for the page
+        setErrors((currentErrorsArray) => {
+          return [...currentErrorsArray, ...stepErrors]
+        })
+        setCustomPageProps(stepCustomPageProps)
+        setUiStage(stage)
+        setUiFeatures(getStageFeatures(lang, overrideStage || stage))
+        setUiElements(step.callbacks)
+        setSubmitData(() => submitDataFunc)
+      }
+    })
+  }, [pageStep, overrideStage, service, token])
+
+  const onSubmit = (evt) => {
+    evt.preventDefault()
+    setErrors([])
+
+    const formData = serializeForm(evt.target.elements)
+    const stepOptions = getStepOptions()
+    submitData(formData, stepOptions)
+  }
+
+  return (
+    <FeatureDynamicView
+      width='two-thirds'
+      onSubmit={onSubmit}
+      hasBackLink={true}
+      hasAccountLinks={true}
+      hasLogoutLink={true}
+      titleLinkHref="/account/home"
+    >
+      <Dynamic
+        {...router.query}
+        {...customPageProps}
+        componentMap={componentMap}
+        headingCount={headingCount}
+        content={uiFeatures}
+        uiElements={uiElements}
+        uiStage={uiStage}
+        errors={errors}
+      />
+    </FeatureDynamicView>
+  )
+}
+
+InviteUser.propTypes = {
+  lang: PropTypes.string
+}
+
+export default withAccessToken(withLang(InviteUser))
--- services/environment.js
+++ services/environment.js
@@ -1,8 +1,8 @@
 // These settings default to dev environment values if no value is present
 export const ENVIRONMENT = process.env.ENVIRONMENT || 'dev'
-export const FORGEROCK_AM = process.env.FORGEROCK_AM || 'https://identity.amido.aws.chdev.org/am/'
-export const FORGEROCK_USER_ENDPOINT = process.env.FORGEROCK_USER_ENDPOINT || 'https://identity.amido.aws.chdev.org/openidm/managed/alpha_user/'
-export const FORGEROCK_COMPANY_ENDPOINT = process.env.FORGEROCK_COMPANY_ENDPOINT || 'https://identity.amido.aws.chdev.org/openidm/managed/alpha_organization/'
+export const FORGEROCK_AM = process.env.FORGEROCK_AM || 'https://idam.amido.aws.chdev.org/am/'
+export const FORGEROCK_USER_ENDPOINT = process.env.FORGEROCK_USER_ENDPOINT || 'https://idam.amido.aws.chdev.org/openidm/managed/alpha_user/'
+export const FORGEROCK_COMPANY_ENDPOINT = process.env.FORGEROCK_COMPANY_ENDPOINT || 'https://idam.amido.aws.chdev.org/openidm/managed/alpha_organization/'
 export const FORGEROCK_REALM = process.env.FORGEROCK_REALM || '/alpha'
 export const FORGEROCK_SCOPE = process.env.FORGEROCK_SCOPE || 'openid email phone profile fr:idm:*'
 export const FORGEROCK_CLIENT_ID = process.env.FORGEROCK_CLIENT_ID || 'ForgeRockSDKClient'
@@ -14,6 +14,7 @@ export const FORGEROCK_TREE_REGISTER_VERIFY = process.env.FORGEROCK_TREE_REGISTE
 export const FORGEROCK_TREE_FMP = process.env.FORGEROCK_TREE_FMP || 'CHResetPassword'
 export const FORGEROCK_TREE_FMP_VERIFY = process.env.FORGEROCK_TREE_FMP_VERIFY || 'CHResetPassword'
 export const FORGEROCK_TREE_COMPANY_ASSOCIATION = process.env.FORGEROCK_TREE_COMPANY_ASSOCIATION || 'CHCompanyAssociation'
+export const FORGEROCK_TREE_INVITE_USER = process.env.FORGEROCK_TREE_INVITE_USER || 'CHInviteUser'
 export const FORGEROCK_TREE_REQUEST_AUTH_CODE = process.env.FORGEROCK_TREE_REQUEST_AUTH_CODE || 'RequestAuthCode'
 export const FORGEROCK_TREE_CHANGE_PASSWORD = process.env.FORGEROCK_TREE_CHANGE_PASSWORD || 'CHChangePassword'
 
