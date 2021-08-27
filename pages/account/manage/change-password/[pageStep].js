f9a7f7dc0c60a5217f2b6dbd275c893211bb5fb2
Stuart Parr <stuart.parr@amido.com>
2021-08-27 15:27:41 +0100

    Refactor change password journey to use FRFlow

--- pages/account/manage/change-password/[pageStep].js
+++ pages/account/manage/change-password/[pageStep].js
@@ -1,24 +1,19 @@
 import PropTypes from 'prop-types'
-import React, { useMemo, useRef } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
 import HeadingCount from '../../../../services/HeadingCount'
-import { findCustomPageProps, findCustomStage, forgerockFlow } from '../../../../services/forgerock'
 import { FORGEROCK_TREE_CHANGE_PASSWORD } from '../../../../services/environment'
 import { useRouter } from 'next/router'
-import { getStageFeatures } from '../../../../services/translate'
 import FeatureDynamicView from '../../../../components/views/FeatureDynamicView'
 import WithLang from '../../../../services/lang/WithLang'
 import componentMap from '../../../../services/componentMap'
 import Dynamic from '../../../../components/Dynamic'
-import { serializeForm } from '../../../../services/formData'
-import { translateErrors } from '../../../../services/errors'
-import log from '../../../../services/log'
 import { generateQueryUrl } from '../../../../services/queryString'
+import useFRFlow from '../../../../services/useFRFlow'
 
 export const getStaticPaths = async () => {
   return {
     paths: [
-      { params: { pageStep: '_start' } },
-      { params: { pageStep: '_restart' } }
+      { params: { pageStep: '_start' } }
     ],
     fallback: false
   }
@@ -31,135 +26,55 @@ export const getStaticProps = async () => {
 const ChangePassword = ({ lang }) => {
   const router = useRouter()
   const formRef = useRef()
-  const [errors, setErrors] = React.useState([])
-  const [customPageProps, setCustomPageProps] = React.useState({})
-  const [uiStage, setUiStage] = React.useState('')
-  const [uiFeatures, setUiFeatures] = React.useState([])
-  const [uiElements, setUiElements] = React.useState([])
-  const [submitData, setSubmitData] = React.useState((formData) => {})
   const headingCount = useMemo(() => new HeadingCount(), [])
-  const onSubmitCallbacks = []
+  const { push, query } = router
+  const { pageStep } = query
 
-  const { pageStep = '', service = '', token, overrideStage = '' } = router.query
-
-  React.useEffect(() => {
-    const journeyName = FORGEROCK_TREE_CHANGE_PASSWORD
-
+  useEffect(() => {
     headingCount.reset()
-    if (!pageStep) return
+  })
 
-    if (pageStep === '_restart') {
-      router.replace('/account/manage/change-password/_start/')
-      return
+  const FRFlowConfig = {
+    journeyName: FORGEROCK_TREE_CHANGE_PASSWORD,
+    journeyNamespace: 'CHANGE_PASSWORD',
+    defaultErrorStage: 'CHANGE_PASSWORD_1',
+    lang,
+    formRef,
+    pageStep,
+    handleSuccess: () => {
+      push('/account/home/')
     }
-
-    setErrors([])
-
-    const stepOptions = {
-      query: {
-        token
-      }
-    }
-
-    log.debug('Staring FR journey', journeyName, stepOptions)
-    forgerockFlow({
-      journeyName,
-      journeyNamespace: 'CHANGE_PASSWORD',
-      lang,
-      stepOptions,
-      onFailure: (errData, newErrors = []) => {
-        // We only get here if there was a fatal error signal from the forgerock client library
-        // all other errors are not considered a failure (such as incorrectly formatted inputs etc
-        // and are handled gracefully by the onUpdateUi function
-        let stage = 'CHANGE_PASSWORD_1'
-        newErrors.forEach((error) => {
-          if (error.stage) {
-            stage = error.stage
-          }
-        })
-        setUiFeatures(getStageFeatures(lang, overrideStage || stage))
-      },
-      onUpdateUi: (step, submitDataFunc, stepErrors = []) => {
-        const stepCustomPageProps = findCustomPageProps(step)
-        const stage = step.payload.stage || findCustomStage(step)
-        step.payload.stage = stage
-
-        if (stepCustomPageProps) {
-          if (stepCustomPageProps.apiError) {
-            // Transform the apiError structure to the app's errors array structure
-            const apiErrorsAsAppErrors = stepCustomPageProps.apiError.errors.map((errorItem) => ({
-              label: errorItem.message
-            }))
-
-            stepErrors.push(...apiErrorsAsAppErrors)
-          }
-        }
-
-        stepCustomPageProps.changeSuccessPath = generateQueryUrl('/account/manage/', {
-          notifyToken: 'changePasswordSuccess'
-        })
-
-        // Update the errors for the page
-        setErrors((currentErrorsArray) => {
-          return [...currentErrorsArray, ...stepErrors]
-        })
-
-        setCustomPageProps(stepCustomPageProps)
-        setUiStage(stage)
-        setUiFeatures(getStageFeatures(lang, overrideStage || stage))
-        setUiElements(step.callbacks)
-        setSubmitData(() => submitDataFunc)
-      }
-    })
-  }, [pageStep, overrideStage, service, token, headingCount, lang, router])
-
-  const onSubmit = (evt) => {
-    evt?.preventDefault()
-
-    // Clear existing errors
-    setErrors([])
-
-    // Get formData from the DOM with callback IDTokens as the key
-    const formData = serializeForm(formRef.current)
-
-    // Execute any submit callbacks defined in the child components and apply returned errors
-    for (const callback of onSubmitCallbacks) {
-      const errors = callback(formData)
-      if (errors.length) {
-        setErrors(translateErrors(errors, lang))
-        return
-      }
-    }
-
-    // Submit FR stage
-    submitData(formData)
   }
+  const { uiFeatures, uiElements, uiStage, stepPageProps, flowHandlers, loading } = useFRFlow(FRFlowConfig)
 
-  // Check if the router has been initialised yet
-  if (!pageStep) return null
+  stepPageProps.changeSuccessPath = generateQueryUrl('/account/manage/', {
+    notifyToken: 'changePasswordSuccess'
+  })
+
+  const { onSubmit, ...restHandlers } = flowHandlers
+  const { errors = [], ...restPageProps } = stepPageProps
 
   return (
     <FeatureDynamicView
       formRef={formRef}
       width='two-thirds'
-      titleLinkHref="/account/home"
       onSubmit={onSubmit}
-      errors={errors}
+      onBack={() => { push('/account/manage') }}
       headingCount={headingCount}
-      uiStage={uiStage}
       hasAccountLinks
       accountLinksItem={6}
     >
       <Dynamic
-        {...customPageProps}
-        {...router.query}
+        {...restPageProps}
+        {...query}
         componentMap={componentMap}
         headingCount={headingCount}
         content={uiFeatures}
         errors={errors}
+        loading={loading}
+        handlers={restHandlers}
         uiElements={uiElements}
         uiStage={uiStage}
-        handlers={{ onSubmitCallbacks }}
       />
     </FeatureDynamicView>
   )
