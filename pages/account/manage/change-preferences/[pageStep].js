a633ac6b344c2aa5ff73327766d171c69c0fdda3
Stuart Parr <stuart.parr@amido.com>
2021-08-27 16:47:14 +0100

    Update change preferences journey to use FR Flow

--- pages/account/manage/change-preferences/[pageStep].js
+++ pages/account/manage/change-preferences/[pageStep].js
@@ -1,16 +1,15 @@
 import PropTypes from 'prop-types'
-import React, { useMemo } from 'react'
+import React, { useMemo, useRef } from 'react'
 import HeadingCount from '../../../../services/HeadingCount'
-import { findCustomPageProps, findCustomStage, forgerockFlow } from '../../../../services/forgerock'
 import { FORGEROCK_TREE_CHANGE_PREFS } from '../../../../services/environment'
 import { useRouter } from 'next/router'
-import { getStageFeatures } from '../../../../services/translate'
 import FeatureDynamicView from '../../../../components/views/FeatureDynamicView'
 import componentMap from '../../../../services/componentMap'
 import Dynamic from '../../../../components/Dynamic'
-import { serializeForm } from '../../../../services/formData'
 import { generateQueryUrl } from '../../../../services/queryString'
 import WithLang from '../../../../services/lang/WithLang'
+import useFRFlow from '../../../../services/useFRFlow'
+import WithQueryParams from '../../../../components/providers/WithQueryParams'
 
 export const getStaticPaths = async () => {
   return {
@@ -26,106 +25,46 @@ export const getStaticProps = async () => {
   return { props: {} }
 }
 
-const ChangePreferences = ({ lang }) => {
+const ChangePreferences = ({ lang, queryParams }) => {
   const router = useRouter()
-  const [errors, setErrors] = React.useState([])
-  const [customPageProps, setCustomPageProps] = React.useState({})
-  const [uiStage, setUiStage] = React.useState('')
-  const [uiFeatures, setUiFeatures] = React.useState([])
-  const [uiElements, setUiElements] = React.useState([])
-  const [submitData, setSubmitData] = React.useState((formData) => {})
+  const formRef = useRef()
   const headingCount = useMemo(() => new HeadingCount(), [])
-  const { pageStep = '', action } = router.query
+  const { push, replace } = router
+  const { pageStep, action } = queryParams
 
   React.useEffect(() => {
-    const journeyName = FORGEROCK_TREE_CHANGE_PREFS
     headingCount.reset()
+  })
 
-    if (!pageStep || !action) return
-
+  React.useEffect(() => {
     if (pageStep === '_restart') {
-      router.replace('/account/manage/change-preferences/_start/')
-      return
+      replace('/account/manage/change-preferences/_start/')
     }
+  }, [pageStep, replace])
 
-    setErrors([])
-
-    const stepOptions = {
-      query: {
-        ForceAuth: true,
-        action
-      }
+  const FRFlowConfig = {
+    journeyName: FORGEROCK_TREE_CHANGE_PREFS,
+    journeyNamespace: 'CHANGE_CONSENT_UPDATES',
+    defaultErrorStage: 'CHANGE_CONSENT_UPDATES',
+    lang,
+    formRef,
+    stepQuery: {
+      ForceAuth: true,
+      action
+    },
+    handleSuccess: () => {
+      push('/account/home/')
     }
-
-    forgerockFlow({
-      journeyName,
-      journeyNamespace: 'CHANGE_CONSENT_UPDATES',
-      lang,
-      stepOptions,
-      onFailure: (errData, newErrors = []) => {
-        // We only get here if there was a fatal error signal from the forgerock client library
-        // all other errors are not considered a failure (such as incorrectly formatted inputs etc
-        // and are handled gracefully by the onUpdateUi function
-        setErrors(newErrors)
-
-        let stage = 'CHANGE_CONSENT_UPDATES'
-        newErrors.forEach((error) => {
-          if (error.stage) {
-            stage = error.stage
-          }
-        })
-        setUiFeatures(getStageFeatures(lang, stage))
-      },
-      onUpdateUi: (step, submitDataFunc, stepErrors = []) => {
-        const stepCustomPageProps = findCustomPageProps(step)
-        const stage = step.payload.stage || findCustomStage(step)
-        step.payload.stage = stage
-
-        if (stepCustomPageProps) {
-          if (stepCustomPageProps.apiError) {
-            // Transform the apiError structure to the app's errors array structure
-            const apiErrorsAsAppErrors = stepCustomPageProps.apiError.errors.map((errorItem) => ({
-              label: errorItem.message
-            }))
-
-            stepErrors.push(...apiErrorsAsAppErrors)
-          }
-        }
-
-        stepCustomPageProps.changeSuccessPath = generateQueryUrl('/account/manage/', {
-          notifyToken: 'changePreferencesSuccess'
-        })
-
-        // Update the errors for the page
-        setErrors((currentErrorsArray) => {
-          return [...currentErrorsArray, ...stepErrors]
-        })
-
-        setCustomPageProps(stepCustomPageProps)
-        setUiStage(stage)
-        setUiFeatures(getStageFeatures(lang, stage))
-        setUiElements(step.callbacks)
-        setSubmitData(() => submitDataFunc)
-      }
-    })
-  }, [pageStep, headingCount, lang, router, action])
-
-  const onSubmit = (evt) => {
-    evt.preventDefault()
-    setErrors([])
-
-    const formData = serializeForm(evt.target)
-    const stepOptions = {
-      query: {
-        ForceAuth: true,
-        action
-      }
-    }
-    submitData(formData, stepOptions)
   }
 
-  // Check if the router has been initialised yet
-  if (!pageStep) return null
+  const { uiFeatures, uiElements, uiStage, stepPageProps, flowHandlers, loading } = useFRFlow(FRFlowConfig)
+
+  stepPageProps.changeSuccessPath = generateQueryUrl('/account/manage/', {
+    notifyToken: 'changePreferencesSuccess'
+  })
+
+  const { onSubmit, ...restHandlers } = flowHandlers
+  const { errors = [], ...restPageProps } = stepPageProps
 
   return (
     <FeatureDynamicView
@@ -133,23 +72,27 @@ const ChangePreferences = ({ lang }) => {
       titleLinkHref="/account/home"
       onSubmit={onSubmit}
       hasAccountLinks
+      formRef={formRef}
       accountLinksItem={6}
     >
       <Dynamic
-        {...customPageProps}
+        {...restPageProps}
+        {...queryParams}
         componentMap={componentMap}
         headingCount={headingCount}
         content={uiFeatures}
         errors={errors}
+        loading={loading}
+        handlers={restHandlers}
         uiElements={uiElements}
         uiStage={uiStage}
-        {...router.query}
+
       />
     </FeatureDynamicView>
   )
 }
 
-export default WithLang(ChangePreferences)
+export default WithQueryParams(WithLang(ChangePreferences))
 
 ChangePreferences.propTypes = {
   lang: PropTypes.string.isRequired
