86070b0e5766fc8c76d3f6ae0bf0b89f09ad3de0
Paul Spence <paul.spence@amido.com>
2021-11-25 11:08:30 +0000

    IDAM-913: filling out the stage and creating new lang tokens

--- services/stages/INVITE_USER_CONFIRM.js
+++ services/stages/INVITE_USER_CONFIRM.js
@@ -4,7 +4,43 @@ const INVITE_USER_CONFIRM = (lang, tokens) => [
   {
     component: 'BrowserTitle',
     props: {
-      title: tokens('')
+      title: tokens('SHARED.whatIsTheAuthorisedPersonsEmailAddress')
+    }
+  },
+  {
+    component: 'Caption',
+    dynamicProps: {
+      children: '${companyName}'
+    }
+  },
+  {
+    component: 'PageHeading',
+    props: {
+      children: tokens('INVITE_USER_CONFIRM.[0].pageHeading.checkTheAuthorisedPersonsEmail')
+    }
+  },
+  {
+    component: 'SummaryList',
+    dynamicProps: {
+      'listItems.1.value': '${emailAddress}'
+    },
+    props: {
+      listItems: [
+        {},
+        {
+          label: tokens('SHARED.email'),
+          desc: 'email',
+          href: '/account/manage/change-email/_start'
+        }
+      ]
+    }
+  },
+  {
+    component: 'Button',
+    props: {
+      children: tokens('SHARED.confirmAndSendEmail'),
+      type: 'submit',
+      testId: 'submitButton'
     }
   }
 ]
