36e39ab2e354dab82a4c5f06eb95be4530515fdf
Paul Spence <paul.spence@amido.com>
2021-12-07 17:15:36 +0000

    IDAM-936: setting up the SCRS exisiting user landing page

---
+++ services/stages/SCRS_EXISTING_USER.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-template-curly-in-string */
+
+const SCRS_EXISTING_USER = (lang, tokens) => [
+  {
+    component: 'BrowserTitle',
+    props: {
+      title: tokens('SCRS.accessYourAccount')
+    }
+  }
+]
+
+export default SCRS_EXISTING_USER
