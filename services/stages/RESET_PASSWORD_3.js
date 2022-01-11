a4e2a8a40748c876d0779360c0055e7a3ce751f3
Paul Spence <paul.spence@amido.com>
2022-01-11 15:03:42 +0000

    IDAM-932: showing the correct content based on email or phone selection

--- services/stages/RESET_PASSWORD_3.js
+++ services/stages/RESET_PASSWORD_3.js
@@ -1,7 +1,14 @@
 /* eslint-disable no-template-curly-in-string */
-import { translate } from '../translate'
+import phoneOtp from './shared/phoneOtp.js'
+import emailOtp from './shared/emailOtp.js'
+
 const RESET_PASSWORD_3 = (lang, tokens) => [
   {
+    conditional: {
+      prop: '${type}',
+      operator: 'eeq',
+      value: 'sms'
+    },
     component: 'BrowserTitle',
     props: {
       title: tokens('SHARED.checkYourPhone')
@@ -9,152 +16,32 @@ const RESET_PASSWORD_3 = (lang, tokens) => [
   },
   {
     conditional: {
-      prop: '${resend}',
-      operator: 'is'
+      prop: '${type}',
+      operator: 'nee',
+      value: 'sms'
     },
-    component: 'NotificationBanner',
+    component: 'BrowserTitle',
     props: {
-      title: tokens('SHARED.success'),
-      heading: tokens('SHARED.textSent'),
-      type: 'success'
+      title: tokens('SHARED.checkYourEmail')
+    }
+  },
+  {
+    conditional: {
+      prop: '${type}',
+      operator: 'eeq',
+      value: 'sms'
     },
-    content: [
-      {
-        component: 'SpanText',
-        props: {
-          children: tokens('SHARED.WeveSentAnotherText')
-        }
-      },
-      {
-        component: 'SpanText',
-        props: {
-          weight: 'bold'
-        },
-        dynamicProps: {
-          children: '${phoneNumber}'
-        }
-      },
-      {
-        component: 'SpanText',
-        props: {
-          children: '. ' + tokens('SHARED.itMayTakeAFewMinutesToArrive')
-        }
-      }
-    ]
+    component: 'Fragment',
+    content: phoneOtp(lang, tokens)
   },
   {
-    component: 'PageHeading',
-    props: {
-      children: tokens('SHARED.checkYourPhone')
-    }
-  },
-  {
-    component: 'BodyText',
-    content: [
-      {
-        component: 'SpanText',
-        props: {
-          children: tokens('SHARED.weveSentYouATextMessageWithASecurityCode')
-        }
-      },
-      {
-        component: 'SpanText',
-        props: {
-          weight: 'bold'
-        },
-        content: [
-          {
-            component: 'PadPhoneNumber',
-            dynamicProps: {
-              phoneNumber: '${phoneNumber}'
-            }
-          }
-        ]
-      },
-      {
-        component: 'SpanText',
-        props: {
-          children: tokens('SHARED.')
-        }
-      }
-    ]
-  },
-  {
-    component: 'BodyText',
-    props: {
-      children: tokens('SHARED.itMayTakeAFewMinutesToArrive')
-    }
-  },
-  {
-    component: 'DisplayUiElements',
-    props: {
-      elementProps: {
-        IDToken3: {
-          label: tokens('SHARED.securityCode'),
-          autoComplete: 'off',
-          type: 'number',
-          suffix: false,
-          fixedWidth: '10',
-          customValidation: [
-            {
-              name: 'required',
-              token: translate(lang, 'OTP_REQUIRED')
-            }
-          ]
-        },
-        IDToken5: {
-          _hidden: true
-        }
-      }
-    }
-  },
-  {
-    component: 'Button',
-    props: {
-      children: tokens('SHARED.continue'),
-      type: 'submit',
-      testId: 'submitButton'
-    }
-  },
-  {
-    component: 'Details',
-    props: {
-      summary: tokens('SHARED.iHaveNotReceivedATextMessage')
+    conditional: {
+      prop: '${type}',
+      operator: 'nee',
+      value: 'sms'
     },
-    content: [
-      {
-        component: 'BodyText',
-        content: [
-          {
-            component: 'SpanText',
-            props: {
-              children: tokens('SHARED.ifItStillHasNotArrivedYouCan')
-            }
-          },
-          {
-            component: 'LinkText',
-            props: {
-              children: tokens('SHARED.askUsToSendYouAnotherTextMessage'),
-              href: '',
-              handler: {
-                name: 'onSecondarySubmit',
-                params: {
-                  target: 'IDToken5',
-                  value: 0,
-                  noValidate: true
-                }
-              }
-            }
-          },
-          {
-            component: 'SpanText',
-            props: {
-              children: tokens('SHARED.')
-            }
-          }
-        ]
-      }
-    ]
+    component: 'Fragment',
+    content: emailOtp(lang, tokens)
   }
 ]
 export default RESET_PASSWORD_3
