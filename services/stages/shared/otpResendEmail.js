9871bfbfefc6c3fb7e83c8d478fd843c7a29771a
Paul Spence <paul.spence@amido.com>
2022-01-17 09:39:37 +0000

    IDAM-1141: updating the resend email text

--- services/stages/shared/otpResendEmail.js
+++ services/stages/shared/otpResendEmail.js
@@ -3,7 +3,7 @@ const otpResendEmail = (lang, tokens) => ([
   {
     component: 'PageHeading',
     props: {
-      children: tokens('SHARED.checkYourEmail')
+      children: tokens('REGISTRATION_RESEND.[0].PageHeading.sendANewEmail')
     }
   },
   {
@@ -22,7 +22,7 @@ const otpResendEmail = (lang, tokens) => ([
       {
         component: 'SpanText',
         props: {
-          children: tokens('SHARED.wellSendAnotherEmail')
+          children: tokens('SHARED.BodyText.weveSentYouAnEmailToWhichContainsASecurityCode')
         }
       },
       {
