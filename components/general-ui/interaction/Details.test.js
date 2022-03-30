14bee286ef585c82565f161c5af9ada8924b2d44
Paul Spence <paul.spence@amido.com>
2022-03-30 10:14:44 +0100

    details matomo tests and tests for the template helper

--- components/general-ui/interaction/Details.test.js
+++ components/general-ui/interaction/Details.test.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Details from './Details'
+import { matomoHelper } from '../../../scripts/cleanAnalytics.js'
+
+jest.mock('../../../scripts/cleanAnalytics.js')
 
 describe('Details', () => {
   it('Renders as details', () => {
@@ -14,5 +17,20 @@ describe('Details', () => {
 
     expect(screen.getByText('Test details label')).toBeInTheDocument()
     expect(screen.getByText('Some details')).toBeInTheDocument()
+  })
+
+  it('has a matomo prop, fires the onClick and tracks an event then a goal', () => {
+    render(<Details label="Test details label" testId='details' matomo={['trackEvent', 'companyNo=12323', 'test']}>Some Details</Details>)
+    const testElement = screen.getByText('Test details label')
+
+    expect(testElement).toBeInTheDocument()
+
+    matomoHelper.mockImplementationOnce(() => ({type: 'trackEvent'}))
+
+    fireEvent.click(testElement)
+
+    matomoHelper.mockImplementationOnce(() => ({type: 'trackGoal'}))
+
+    fireEvent.click(testElement)
   })
 })
