49ed426590ba9e015d8b95f316d3b8b7fff59ab9
Paul Spence <paulspence@amido.com>
2022-09-05 17:07:35 +0100

    IDAM-1410: cleaning the console log

--- components/application-specific/BrowserTitle.js
+++ components/application-specific/BrowserTitle.js
@@ -21,7 +21,6 @@ const BrowserTitle = ({ title, errors }) => {
   /* eslint-disable react-hooks/exhaustive-deps */
   React.useEffect(() => {
     const currentUrl = window.location.href
-    console.log('currentUrl: ', cleanAnalytics([currentUrl]))
     pushInstruction('setCustomUrl', [cleanAnalytics([currentUrl])[0]])
     pushInstruction('setDocumentTitle', [title + suffix])
     trackPageView({
