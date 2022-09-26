fad52f83c33576593e32defbde77f8c63fba97fe
Paul Spence <paulspence@amido.com>
2022-09-26 12:06:42 +0100

    IDAM-1434: adding support for hiding the notification

--- components/general-ui/typeography/NotificationBanner.js
+++ components/general-ui/typeography/NotificationBanner.js
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
 import HeadingText from './HeadingText'
 
 const NotificationBanner = (props) => {
   const { type, title, heading, children, className, headingCount, testId, notifyId } = props
   const classes = [className]
+
+  const [hidden, setHidden] = useState(false)
+
+  const onClick = () => setHidden(!hidden)
 
   if (type === 'success') classes.push('govuk-notification-banner--success')
 
@@ -12,15 +16,18 @@ const NotificationBanner = (props) => {
 
   return (
     <div className={`govuk-notification-banner ${finalClassName}`} role="alert" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner" data-testid={testId} data-notificationid={notifyId}>
-      <div className="govuk-notification-banner__header">
+      <div className={`govuk-notification-banner__header ${hidden ? 'govuk-notification-banner__hidden' : ''}`}>
         <HeadingText headingCount={headingCount} size="l" className="govuk-notification-banner__title" id="govuk-notification-banner-title">
           {title}
         </HeadingText>
+        <button className="ch-show-hide-button" onClick={onClick}>{hidden ? 'Show message' : 'Hide message'}</button>
       </div>
-      <div className="govuk-notification-banner__content">
-        <HeadingText headingCount={headingCount} size="l" className="govuk-notification-banner__heading">{heading}</HeadingText>
-        {children && <p className="govuk-body">{children}</p>}
-      </div>
+      {
+        <div className={hidden ? 'govuk-visually-hidden' : 'govuk-notification-banner__content'}>
+          <HeadingText headingCount={headingCount} size="l" className="govuk-notification-banner__heading">{heading}</HeadingText>
+          {children && <p className="govuk-body">{children}</p>}
+        </div>
+      }
     </div>
   )
 }
