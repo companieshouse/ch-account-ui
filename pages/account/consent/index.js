4461f4077ca39ea11c26938ff2ba082caba6b489
Paul Spence <paulspence@amido.com>
2022-11-30 16:47:43 +0000

    consent: adding debug logging and tidy up

--- pages/account/consent/index.js
+++ pages/account/consent/index.js
@@ -1,43 +1,34 @@
 import PropTypes from 'prop-types'
 import React, { useMemo, useEffect, useRef } from 'react'
 import { useRouter } from 'next/router'
-import HeadingCount from '../../../../services/HeadingCount'
+import HeadingCount from '../../../services/HeadingCount'
 import {
-  CH_EWF_LEGACY_AUTH_URL,
   CH_CONSENT
-} from '../../../../services/environment'
-import FeatureDynamicView from '../../../../components/views/FeatureDynamicView'
-import WithLang from '../../../../services/lang/WithLang'
-import componentMap from '../../../../services/componentMap'
-import Dynamic from '../../../../components/Dynamic'
-import withQueryParams from '../../../../components/providers/WithQueryParams'
-import useFRFlow from '../../../../services/useFRFlow'
-import { generateQueryUrl } from '../../../../services/queryString'
-import { mapCompanyData } from '../../../../services/mappings'
-import log from '../../../../services/log'
+} from '../../../services/environment'
+import FeatureDynamicView from '../../../components/views/FeatureDynamicView'
+import WithLang from '../../../services/lang/WithLang'
+import componentMap from '../../../services/componentMap'
+import Dynamic from '../../../components/Dynamic'
+import withQueryParams from '../../../components/providers/WithQueryParams'
+import useFRFlow from '../../../services/useFRFlow'
+import log from '../../../services/log'
 
 const CHConsent = ({ lang, queryParams }) => {
   const router = useRouter()
   const formRef = useRef()
   const headingCount = useMemo(() => new HeadingCount(), [])
-  const { asPath, push } = router
+  const { push } = router
 
   const {
     goto,
     authIndexValue,
     ForceAuth,
-    mode,
-    companyNo,
-    jurisdiction
+    mode
   } = queryParams
-
-  log.debug('CHS queryParams 123: ', queryParams)
 
   useEffect(() => {
     headingCount.reset()
   })
-
-  log.debug('CHS ForceAuth: ', ForceAuth)
 
   const FRFlowConfig = {
     journeyName: authIndexValue || CH_CONSENT,
@@ -47,64 +38,32 @@ const CHConsent = ({ lang, queryParams }) => {
     lang,
     formRef,
     stepQuery: {
-      companyNo,
+      goto,
+      authIndexValue,
       ForceAuth,
-      jurisdiction
+      mode
     },
     handleSuccess: (branch) => {
       if (goto) {
-        // CHLogin journey ONLY
-        // does the user have a session, if so send them to the redirect_uri
-        if (branch === '/hassession') {
-          let backToApp = ''
-          const params = goto.split('&')
-
-          params.map(param => {
-            const split = param.split('=')
-            if (split[0] === 'redirect_uri') {
-              log.debug('push to ', split[1])
-              backToApp = split[1]
-            }
-
-            return split.join('=')
-          })
-
-          if (backToApp !== '') {
-            return push(backToApp)
-          }
-        } else {
-          return push(goto)
-        }
+        return push(goto)
       }
+      log.debug('CONSENT JOURNEY COMPLETE AND SUCCESSFULL')
+      log.debug('branch: consent: ', branch)
     }
   }
+
+  log.debug(FRFlowConfig)
 
   const { uiFeatures, uiElements, uiStage, stepPageProps, flowHandlers, loading } = useFRFlow(FRFlowConfig)
 
   const { onSubmit, ...restHandlers } = flowHandlers
 
-  const isCompanySelection = uiStage === 'EWF_LOGIN_2' || uiStage === 'EWF_LOGIN_3' || uiStage === 'EWF_LOGIN_4' || uiStage === 'EWF_LOGIN_5'
-
-  const onBack = (evt) => {
-    evt.preventDefault()
-    const home = isCompanySelection ? '/account/home/' : '/account/CHConsent/'
-    window.location.assign(authIndexValue === CH_CONSENT ? asPath : home)
-  }
-
-  const links = {
-    chooseCompanyPath: `${asPath}`,
-    requestAuthCodePath: generateQueryUrl('/account/request-auth-code', { companyName: stepPageProps.company?.name }),
-    ewfLegacyAuthUrl: CH_EWF_LEGACY_AUTH_URL,
-    resumePath: authIndexValue === CH_CONSENT ? asPath : '/account/CHConsent/sdfasdfasdfasd'
-  }
-
-  const { errors = [], company, ...restPageProps } = stepPageProps
+  const { errors = [], ...restPageProps } = stepPageProps
 
   return (
     <FeatureDynamicView
       onSubmit={onSubmit}
       formRef={formRef}
-      onBack={onBack}
       hasBackLink={false}
     >
       {uiStage
@@ -113,11 +72,9 @@ const CHConsent = ({ lang, queryParams }) => {
         {...queryParams}
         componentMap={componentMap}
         content={uiFeatures}
-        company={company ? mapCompanyData(company) : null}
         errors={errors}
         handlers={restHandlers}
         headingCount={headingCount}
-        links={links}
         loading={loading}
         uiElements={uiElements}
         uiStage={uiStage}
