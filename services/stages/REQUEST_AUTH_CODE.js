c4a13830d1665677d8565414e53ed840abdbd9cd
Dan Beck <46429371+DanBeckDev@users.noreply.github.com>
2023-05-04 16:30:41 +0100

    fix: remove company name from matomo (#574)
    PBI: https://companieshouse.atlassian.net/browse/IDAM-1594
    
    WHAT
    We need to ensure we do not capture PII data in the service analytics
    
    WHY
    So that we do not breach our corporate data privacy requirements
    
    HOW
    pass the text without the company name into the analytics

--- services/stages/REQUEST_AUTH_CODE.js
+++ services/stages/REQUEST_AUTH_CODE.js
@@ -30,7 +30,7 @@ const REQUEST_AUTH_CODE = (lang, tokens) => [
           href: '',
           target: '_blank',
           children: tokens('REQUEST_AUTHENTICATION_CODE_1.[5].LinkText.sendAuthenticationCodeToOffice'),
-          matomo: ['trackEvent', tokens('REQUEST_AUTHENTICATION_CODE_1.[1].PageHeading.youNeedTheAuthenticationCode'), tokens('REQUEST_AUTHENTICATION_CODE_1.[5].LinkText.sendAuthenticationCodeToOffice')]
+          matomo: ['trackEvent', tokens('REQUEST_AUTHENTICATION_CODE_1.[1].PageHeading.youNeedTheAuthenticationCode.analytics'), tokens('REQUEST_AUTHENTICATION_CODE_1.[5].LinkText.sendAuthenticationCodeToOffice')]
         }
       }
     ]
@@ -47,7 +47,7 @@ const REQUEST_AUTH_CODE = (lang, tokens) => [
           href: '',
           target: '_blank',
           children: tokens('REQUEST_AUTHENTICATION_CODE_1.[6].LinkText.sendAuthenticationCodeToHome'),
-          matomo: ['trackEvent', tokens('REQUEST_AUTHENTICATION_CODE_1.[1].PageHeading.youNeedTheAuthenticationCode'), tokens('REQUEST_AUTHENTICATION_CODE_1.[6].LinkText.sendAuthenticationCodeToHome')]
+          matomo: ['trackEvent', tokens('REQUEST_AUTHENTICATION_CODE_1.[1].PageHeading.youNeedTheAuthenticationCode.analytics'), tokens('REQUEST_AUTHENTICATION_CODE_1.[6].LinkText.sendAuthenticationCodeToHome')]
         }
       }
     ]
@@ -62,7 +62,7 @@ const REQUEST_AUTH_CODE = (lang, tokens) => [
       renderAs: 'link',
       href: '/account/home/',
       testId: 'homeButton',
-      matomo: ['trackEvent', tokens('REQUEST_AUTHENTICATION_CODE_1.[1].PageHeading.youNeedTheAuthenticationCode'), tokens('REQUEST_AUTHENTICATION_CODE_1.[6].Button.returnToHomePage')]
+      matomo: ['trackEvent', tokens('REQUEST_AUTHENTICATION_CODE_1.[1].PageHeading.youNeedTheAuthenticationCode.analytics'), tokens('REQUEST_AUTHENTICATION_CODE_1.[6].Button.returnToHomePage')]
     }
   }
 ]
