ad06d20a09fa0cfcfefc4f124f57e40aa1b74bad
jideboris <jideboris@yahoo.com>
2023-06-04 15:00:10 +0100

    Add to inlude pagination on your companies page
    Add to inlude pagination on your companies page
    
    Add useFRAuthpagination and pagination on the page
    
    Add useFRAuthpagination and pagination on the page
    
    Add to fix loading bug
    
    Add error handling
    
    Fix accessibility issues
    
    fix navigation issue
    
    Fix search functionality
    
    Fix search functionality
    
    Add to fix dev warning message
    
    fix unwanted re-rendering
    
    fix unwanted re-rendering

--- components/general-ui/interaction/Pagination.test.js
+++ components/general-ui/interaction/Pagination.test.js
@@ -1,16 +1,36 @@
 import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Pagination from './Pagination'
 
 jest.mock('../../../scripts/cleanAnalytics.js')
 
 describe('Pagination', () => {
-  it('Renders as Pagination component, default behaviour', () => {
-    render(<Pagination />)
-
+  it('Renders as Pagination component, when display is true', () => {
+    render(<Pagination display={true} />)
     const testElement = screen.getByTestId('nav-id')
-
+   
     expect(testElement).toBeInTheDocument()
   })
 
+  it('Should not renders as Pagination component, when display is false', () => {
+    render(<Pagination display={false} />)
+    const testElement = screen.getByTestId('div-id')
+   
+    expect(testElement).toBeInTheDocument()
+    expect(testElement.innerHTML).toBe("")
+  })
+
+  it('Should renders as Pagination Next Button, when displayNext is true', () => {
+    render(<Pagination display={true} displayNext={true}/>)
+    const testElement = screen.getByTestId('next-id')
+   
+    expect(testElement).toBeInTheDocument()
+  })
+
+  it('Should renders as Pagination Previous Button, when displayPrevious is true', () => {
+    render(<Pagination display={true} displayPrev={true}/>)
+    const testElement = screen.getByTestId('previous-id')
+   
+    expect(testElement).toBeInTheDocument()
+  })
 })
