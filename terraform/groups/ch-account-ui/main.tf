97747e3987ccc2fd6d228603582209d101d08db2
Robert Stettner <5081777+robertstettner@users.noreply.github.com>
2023-06-14 15:26:04 +0100

    Remove vulnerability with FE S3 Bucket (#589)

--- terraform/groups/ch-account-ui/main.tf
+++ terraform/groups/ch-account-ui/main.tf
@@ -28,9 +28,7 @@ resource "aws_s3_bucket" "website" {
 data "aws_iam_policy_document" "website" {
   statement {
     actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:DeleteObject"
+      "s3:GetObject"
     ]
     principals {
       identifiers = ["*"]
