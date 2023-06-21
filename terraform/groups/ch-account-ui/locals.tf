b02395212f582d3c1407b6687795884f59eceb97
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2023-06-21 14:27:13 +0100

    Update local to incorporate additional ARNs

--- terraform/groups/ch-account-ui/locals.tf
+++ terraform/groups/ch-account-ui/locals.tf
@@ -10,5 +10,9 @@ locals {
 
   secrets                        = jsondecode(data.vault_generic_secret.secrets.data_json)
   concourse_role_arn_list        = local.secrets["concourse_role_arn_list"]
-  website_iam_policy_identifiers = local.concourse_role_arn_list
+  concourse_user_arn_list        = local.secrets["concourse_user_arn_list"]
+  website_iam_policy_identifiers = concat(
+    local.concourse_role_arn_list,
+    local.concourse_user_arn_list
+  )
 }
