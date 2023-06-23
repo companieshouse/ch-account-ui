e136b654fc0ac2cfb2732734e57997216aae937d
Dan Beck <46429371+DanBeckDev@users.noreply.github.com>
2023-06-23 10:58:27 +0100

    Revert "Revert "fix: provide the right data for auth person companies page (#588)" (#595)"
    This reverts commit afc7b4a244e6e247514ba3f8032881a7e8d45250.

--- services/forgerock.js
+++ services/forgerock.js
@@ -442,7 +442,8 @@ export const getCompaniesAssociatedWithUser = async (accessToken, userId, compan
             currentUser: member._id === userId,
             detailsPath: generateQueryUrl('/account/your-companies/authorised-person', {
               companyNumber: company.number,
-              userId: member._id
+              userId: member._id,
+              page: currentPage
             })
           })
           )
